46e8fe8461ebf54e2e0b3483d66714b6353309db
XanTheDragon <subzeroxan@gmail.com>
2018-10-13 17:45:40 -0500

    Update SocialHandler.cs

--- Source/NexusForever.WorldServer/Network/Message/Handler/SocialHandler.cs
+++ Source/NexusForever.WorldServer/Network/Message/Handler/SocialHandler.cs
@@ -1,19 +1,23 @@
-﻿using NexusForever.Shared.Network.Message;
+using System;
+using NexusForever.Shared.Network.Message;
 using NexusForever.WorldServer.Command;
 using NexusForever.WorldServer.Network.Message.Model;
 
-namespace NexusForever.WorldServer.Network.Message.Handler
-{
-    public static class SocialHandler
-    {
+namespace NexusForever.WorldServer.Network.Message.Handler {
+    public static class SocialHandler {
+
+        public static readonly string commandPrefix = "!";
+
         [MessageHandler(GameMessageOpcode.ClientChat)]
-        public static void HandleChat(WorldSession session, ClientChat chat)
-        {
-            if (chat.Message.StartsWith("!"))
-            {
-                CommandManager.ParseCommand(chat.Message, out string command, out string[] parameters);
-                CommandHandlerDelegate handler = CommandManager.GetCommandHandler(command);
-                handler?.Invoke(session, parameters);
+        public static void HandleChat(WorldSession session, ClientChat chat) {
+            if (chat.Message.StartsWith(commandPrefix)) {
+                try {
+                    CommandManager.ParseCommand(chat.Message, out string command, out string[] parameters);
+                    CommandHandlerDelegate handler = CommandManager.GetCommandHandler(command);
+                    handler?.Invoke(session, parameters);
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
             }
         }
     }
