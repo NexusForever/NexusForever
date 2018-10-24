678b14080c2113d49da12d19a6d54c61614cccdf
warsongkiller <warsongkiller.s8@gmail.com>
2018-10-24 22:14:58 +0200

    Fix ServerMountUnlocked

--- Source/NexusForever.WorldServer/Network/Message/Model/ServerMountUnlocked.cs
+++ Source/NexusForever.WorldServer/Network/Message/Model/ServerMountUnlocked.cs
@@ -4,12 +4,13 @@ using NexusForever.Shared.Network.Message;
 namespace NexusForever.WorldServer.Network.Message.Model
 {
     [Message(GameMessageOpcode.ServerMountUnlocked, MessageDirection.Server)]
-
     public class ServerMountUnlocked : IWritable
     {
-        public void Write(GamePacketWriter writer)
+        public uint SpellId { get; set; }
+
+        public void Write (GamePacketWriter writer)
         {
-            writer.WriteBytes(new byte[18]);
+            writer.Write(SpellId, 18);
         }
     }
 }
