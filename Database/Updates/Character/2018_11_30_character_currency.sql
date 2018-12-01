503d0938c01abe59ca9ff8796de4800fed97b8ac
Matt Hurd <sabahbob@gmail.com>
2018-12-01 11:30:30 -0600

    Change count to amount in database

--- Database/Updates/Character/2018_11_30_character_currency.sql
+++ Database/Updates/Character/2018_11_30_character_currency.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `character_currency` (
     `characterId` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
     `currencyId` TINYINT(4) UNSIGNED NOT NULL DEFAULT '0',
-    `count` UNSIGNED BIGINT(20) NOT NULL DEFAULT '0',
+    `amount` UNSIGNED BIGINT(20) NOT NULL DEFAULT '0',
     PRIMARY KEY (`id`, `currencyId`),
     CONSTRAINT `FK_character_currency_id__character_id` FOREIGN KEY (`id`) REFERENCES `character` (`id`) ON DELETE CASCADE
 );
