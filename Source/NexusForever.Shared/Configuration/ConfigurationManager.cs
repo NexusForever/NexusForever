62d6ec047a9bcad7b86c3ba558c0bc9b5a2be1d1
Jason Couture <jcouture@pssproducts.com>
2018-12-02 09:34:49 -0500

    Fully converted to ConfiguraitonBuilder instead of json.net Testing resharper changes.

--- Source/NexusForever.Shared/Configuration/ConfigurationManager.cs
+++ Source/NexusForever.Shared/Configuration/ConfigurationManager.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Linq;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace NexusForever.Shared.Configuration
 {
+
     public static class ConfigurationManager<T>
     {
         public static T Config { get; private set; }
+
         public static IConfiguration Configuration { get; private set; }
+
 
         public static void Initialise(string file)
         {
-            ConfigurationBuilder builder = new ConfigurationBuilder();
-
-            builder
-                .AddJsonFile(file, false, true)
-                .AddEnvironmentVariables()
-                .AddCommandLine(Environment.GetCommandLineArgs().Skip(1).ToArray());
-
-            Configuration = builder.Build();
+            SharedConfiguration.Initialize(file);
             Config = Configuration.Get<T>();
         }
     }
