7f4cf7e5c89d51385746f4016e89dd77ba2a3dbd
Matt Hurd <sabahbob@gmail.com>
2018-12-03 18:27:14 -0600

    fix migration to have proper primary keys

--- Database/Updates/Character/2018_11_30_character_currency.sql
+++ Database/Updates/Character/2018_11_30_character_currency.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `character_currency` (
     `characterId` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
     `currencyId` TINYINT(4) UNSIGNED NOT NULL DEFAULT '0',
     `amount` UNSIGNED BIGINT(20) NOT NULL DEFAULT '0',
-    PRIMARY KEY (`id`, `currencyId`),
-    CONSTRAINT `FK_character_currency_id__character_id` FOREIGN KEY (`id`) REFERENCES `character` (`id`) ON DELETE CASCADE
+    PRIMARY KEY (`characterId`, `currencyId`),
+    CONSTRAINT `FK_character_currency_id__character_id` FOREIGN KEY (`characterId`) REFERENCES `character` (`id`) ON DELETE CASCADE
 );
