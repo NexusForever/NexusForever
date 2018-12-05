7bbfff3474aa578273eb41e54a2fb68853fb9e24
root <vinz@trans.am>
2018-12-06 00:32:42 +0100

    content error checking

--- Source/NexusForever.WorldServer/Game/Entity/NonPlayer.cs
+++ Source/NexusForever.WorldServer/Game/Entity/NonPlayer.cs
@@ -57,23 +57,30 @@ namespace NexusForever.WorldServer.Game.Entity
         private void CalculateProperties()
         {
             Creature2Entry creature2Entry = GameTableManager.Creature2.GetEntry(CreatureId);
-            UnitRaceEntry unitRaceEntry = GameTableManager.UnitRace.GetEntry(creature2Entry.UnitRaceId);
-            Creature2DifficultyEntry creature2DifficultyEntry = GameTableManager.Creature2Difficulty.GetEntry(creature2Entry.Creature2DifficultyId);
-            Creature2ArcheTypeEntry creature2ArcheTypeEntry = GameTableManager.Creature2ArcheType.GetEntry(creature2Entry.Creature2ArcheTypeId);
-            Creature2TierEntry creature2TierEntry = GameTableManager.Creature2Tier.GetEntry(creature2Entry.Creature2TierId);
-            Creature2ModelInfoEntry creature2ModelInfoEntry = GameTableManager.Creature2ModelInfo.GetEntry(creature2Entry.Creature2ModelInfoId);
+
+            if (creature2Entry == null||creature2Entry.Id<1||creature2Entry.Creature2DifficultyId<1||creature2Entry.Creature2ArcheTypeId<1||creature2Entry.Creature2TierId<1||creature2Entry.UnitRaceId<1)
+                return;
+
+            UnitRaceEntry unitRaceEntry                                = GameTableManager.UnitRace.GetEntry(creature2Entry.UnitRaceId);
+            Creature2DifficultyEntry creature2DifficultyEntry          = GameTableManager.Creature2Difficulty.GetEntry(creature2Entry.Creature2DifficultyId);
+            Creature2ArcheTypeEntry creature2ArcheTypeEntry            = GameTableManager.Creature2ArcheType.GetEntry(creature2Entry.Creature2ArcheTypeId);
+            Creature2TierEntry creature2TierEntry                      = GameTableManager.Creature2Tier.GetEntry(creature2Entry.Creature2TierId);
+            Creature2ModelInfoEntry creature2ModelInfoEntry            = GameTableManager.Creature2ModelInfo.GetEntry(creature2Entry.Creature2ModelInfoId);
             Creature2DisplayGroupEntryEntry creature2DisplayGroupEntry = GameTableManager.Creature2DisplayGroupEntry.GetEntry(creature2Entry.Creature2DisplayGroupId);
-            Creature2OutfitGroupEntryEntry creature2OutfitGroupEntry = GameTableManager.Creature2OutfitGroupEntry.GetEntry(creature2Entry.Creature2OutfitGroupId);
-            Faction2Entry faction2Entry = GameTableManager.Faction2.GetEntry(creature2Entry.FactionId);
+            Creature2OutfitGroupEntryEntry creature2OutfitGroupEntry   = GameTableManager.Creature2OutfitGroupEntry.GetEntry(creature2Entry.Creature2OutfitGroupId);
+            Faction2Entry faction2Entry                                = GameTableManager.Faction2.GetEntry(creature2Entry.FactionId);
 
             int level = new System.Random().Next((int)creature2Entry.MinLevel, (int)creature2Entry.MaxLevel);
-            CreatureLevelEntry creatureLevelEntry = GameTableManager.CreatureLevel.GetEntry((ulong)level);
+            CreatureLevelEntry creatureLevelEntry                      = GameTableManager.CreatureLevel.GetEntry((ulong)level);
 
             float values;
 
             for (uint i = 0u; i < creatureLevelEntry.UnitPropertyValue.Length; i++)
             {
-                values = creatureLevelEntry.UnitPropertyValue[i] * creature2ArcheTypeEntry.UnitPropertyMultiplier[i] * creature2DifficultyEntry.UnitPropertyMultiplier[i] * creature2TierEntry.UnitPropertyMultiplier[i];
+                values = creatureLevelEntry.UnitPropertyValue[i]
+                        * creature2ArcheTypeEntry.UnitPropertyMultiplier[i]
+                        * creature2DifficultyEntry.UnitPropertyMultiplier[i]
+                        * creature2TierEntry.UnitPropertyMultiplier[i];
                 SetProperty((Property)i, values);
             }
             Stats.Add(Stat.Level, new StatValue(Stat.Level, (uint)level));
