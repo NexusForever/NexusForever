aa760d976f46cca34b52f4ed0682a968fef07b2a
Kirmmin <kirmmin@hotmail.com>
2018-12-09 14:53:12 -0800

    Fix typo in enum comment

--- Source/NexusForever.WorldServer/Game/Entity/Static/PvPFlag.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Static/PvPFlag.cs
@@ -4,7 +4,7 @@
     {
         Disabled    = 0,
         Enabled     = 1,
-        Forced      = 2, // Seems to be forced to server settings. Toggle "on" is disabled on PvP server. Possibly a city thing?
+        Forced      = 2, // Seems to be forced to server settings. Toggle "on" is disabled on PvE server. Possibly a city thing?
         Unknown     = 6 // Same deal as PvPFlag.Forced.
     }
 }
