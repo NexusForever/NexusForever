82e3c97bf699960bdf52e2c9aec20ee82f25f7cb
Kirmmin <kirmmin@hotmail.com>
2018-12-09 15:31:43 -0800

    Stat.cs update for Shield

--- Source/NexusForever.WorldServer/Game/Entity/Static/Stat.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Static/Stat.cs
@@ -2,13 +2,13 @@
 {
     public enum Stat
     {
-        Health  = 0,
-        Dash  = 9,
-        Level = 10,
-        MentorLevel = 11,
-        State = 12, // 0 = Standing (Combat Pose), 1 = Sitting, 2 = Laying Down, 3 = Standing (Idle)
-        Sheathed = 15, // 0 = Unsheathed Weapons, 1 = Sheathed Weapons
+        Health              = 0,
+        Dash                = 9,
+        Level               = 10,
+        MentorLevel         = 11,
+        State               = 12, // 0 = Standing (Combat Pose), 1 = Sitting, 2 = Laying Down, 3 = Standing (Idle)
+        Sheathed            = 15, // 0 = Unsheathed Weapons, 1 = Sheathed Weapons
+        Shield              = 20,
         LowHpAndGhostEffect = 32, // Need confirmation what screen effects are happening
-        StateDuplicate = 44, // See State (12)
     }
 }
