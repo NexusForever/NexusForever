f67c645683c1d47590f448cac0e20dc96c21ff77
Skyler <rawahodev@gmail.com>
2018-12-13 05:22:24 +1300

    Update Language.cs
    Sync with Language.tbl

--- Source/NexusForever.Shared/GameTable/Static/Language.cs
+++ Source/NexusForever.Shared/GameTable/Static/Language.cs
@@ -1,10 +1,10 @@
 ﻿namespace NexusForever.Shared.GameTable.Static
 {
-    public enum Language : uint
+    public enum Language
     {
         English = 1,
-        German = 2,
-        French,
-        Korean
+        German  = 2,
+        French  = 3,
+        Chinese = 5
     }
 }
