dcd63ee0b6fbac8135133b98889c027a85b4abf0
Jason Couture <plaguethenet@gmail.com>
2018-12-13 07:53:44 -0500

    Initial support for logout from both client and server. (#58)
    * Initial support for logout from both client and server.
    TODO: Event system will need to be reworked to enforce logout delay.
    
    * Squash missed a file.
    
    * Moved logout handler into CharacterHandler

---
+++ Source/NexusForever.WorldServer/Network/Message/Model/ServerClientLogout.cs
@@ -0,0 +1,19 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+
+    [Message(GameMessageOpcode.ServerClientLogout, MessageDirection.Server)]
+    public class ServerClientLogout : IWritable
+    {
+        public bool ClientRequested { get; set; }
+        public LogoutReason Reason { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(ClientRequested);
+            writer.Write(Reason, 5);
+        }
+    }
+}
