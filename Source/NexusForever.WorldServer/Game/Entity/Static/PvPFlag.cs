b768990c122f4d4c771646cd8a10c5687331d551
Kirmmin <kirmmin@hotmail.com>
2018-12-13 06:54:32 -0800

    Updated PvPFlag Enum to be a [Flag] You can force a player's PvP state by combining flags

--- Source/NexusForever.WorldServer/Game/Entity/Static/PvPFlag.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Static/PvPFlag.cs
@@ -1,10 +1,15 @@
-﻿namespace NexusForever.WorldServer.Game.Entity.Static
+﻿using System;
+
+namespace NexusForever.WorldServer.Game.Entity.Static
 {
+    /// <summary>
+    /// Determines which fields need saving for <see cref="Player"/> when being saved to the database.
+    /// </summary>
+    [Flags]
     public enum PvPFlag
     {
         Disabled    = 0,
         Enabled     = 1,
-        Forced      = 2, // Seems to be forced to server settings. Toggle "on" is disabled on PvE server. Possibly a city thing?
-        Unknown     = 6 // Same deal as PvPFlag.Forced.
+        Forced      = 2, // Disables "Turn On/Off PvP" in Portrait.
     }
 }
