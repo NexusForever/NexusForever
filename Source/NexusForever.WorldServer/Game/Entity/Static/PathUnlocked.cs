8606b90ee526a1b40db94269dcd077b852ea0563
Kirmmin <kirmmin@hotmail.com>
2018-12-13 19:27:31 -0800

    Added PathUnlocked enum Remove large block comment

---
+++ Source/NexusForever.WorldServer/Game/Entity/Static/PathUnlocked.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NexusForever.WorldServer.Game.Entity.Static
+{
+    /// <summary>
+    /// Allows a flag to unlock <see cref="Path"/> for <see cref="Player"/>.
+    /// </summary>
+    [Flags]
+    public enum PathUnlocked
+    {
+        Soldier     = 1,
+        Settler     = 2,
+        Scientist   = 4,
+        Explorer    = 8
+    }
+}
