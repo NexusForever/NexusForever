c687fc598f202063a1c367c83ab63dc65f2b365e
Jason Couture <jcouture@pssproducts.com>
2018-12-30 17:07:50 -0500

    Fixed issues preventing go command from working. Issue #1: French was not loading correctly due to multi-byte unicode code points. Issue #2: File cache was loading old data, added code to clean the cache if the binary changes. Issue #3: Go command search was only returning a single ID, when there may be multiple. Issue #4: *.bin has multiple ID's for a single string in some instances, the index was not accounting for this, which caused some random lookup failures.

--- Source/NexusForever.Shared/Cryptography/FileHasher.cs
+++ Source/NexusForever.Shared/Cryptography/FileHasher.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace NexusForever.Shared.Cryptography
 {
-    public static class FileHasher
+    public static class Hasher
     {
-        public static string HashFile(string fileName)
+        public static string Hash(this Stream stream)
         {
-            using (FileStream fileStream = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (SHA256 sha256 = SHA256.Create())
             {
-                return BitConverter.ToString(sha256.ComputeHash(fileStream))
+                return BitConverter.ToString(sha256.ComputeHash(stream))
                     .Replace("-", "")
                     .ToLower();
             }
+        }
+        public static string HashBytes(byte[] data)
+        {
+            using (var memoryStream = new MemoryStream(data))
+                return memoryStream.Hash();
+        }
+
+        public static string Hash(this string @string, Encoding encoding = null)
+        {
+            if(encoding == null)
+                encoding = Encoding.UTF8;
+            return HashBytes(encoding.GetBytes(@string));
+        }
+
+        public static string HashFile(string fileName)
+        {
+            using (FileStream fileStream = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                return fileStream.Hash();
+
         }
     }
 }
--- Source/NexusForever.WorldServer/Command/Handler/GoHandler.cs
+++ Source/NexusForever.WorldServer/Command/Handler/GoHandler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using NexusForever.Shared.GameTable;
@@ -11,24 +12,32 @@ namespace NexusForever.WorldServer.Command.Handler
     public class GoHandler : NamedCommand
     {
         public GoHandler()
-            : base(true, "go")
+            : base(false, "go")
         {
         }
 
+        private IEnumerable<uint> GetTextIds(WorldLocation2Entry entry)
+        {
+            WorldZoneEntry worldZone = GameTableManager.WorldZone.GetEntry(entry.WorldZoneId);
+            if (worldZone != null && worldZone.LocalizedTextIdName != 0)
+                yield return worldZone.LocalizedTextIdName;
+            WorldEntry world = GameTableManager.World.GetEntry(entry.WorldId);
+            if (world != null && world.LocalizedTextIdName != 0)
+                yield return world.LocalizedTextIdName;
+        }
         protected override async Task HandleCommandAsync(CommandContext context, string command, string[] parameters)
         {
             string zoneName = string.Join(" ", parameters);
 
-            WorldLocation2Entry zone = SearchManager.Search<WorldLocation2Entry>(zoneName, context.Language, 
-                i => 
-                GameTableManager.WorldZone.GetEntry(i.WorldZoneId)?.LocalizedTextIdName ?? 
-                GameTableManager.World.GetEntry(i.WorldId)?.LocalizedTextIdName ?? 
-                0).FirstOrDefault();
+            WorldLocation2Entry zone = SearchManager.Search<WorldLocation2Entry>(zoneName, context.Language, GetTextIds).FirstOrDefault();
+
             if (zone == null)
                 await context.SendErrorAsync($"Unknown zone: {zoneName}");
             else
-                context.Session.Player.TeleportTo((ushort)zone.WorldId, zone.Position0, zone.Position1,
-                    zone.Position2);
+            {
+                context.Session?.Player.TeleportTo((ushort)zone.WorldId, zone.Position0, zone.Position1, zone.Position2);
+                await context.SendMessageAsync($"{zoneName}: {zone.WorldId} {zone.Position0} {zone.Position1} {zone.Position2}");
+            }
         }
     }
 }
