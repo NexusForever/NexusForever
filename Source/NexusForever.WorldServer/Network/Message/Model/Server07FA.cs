d2136fd09f7703218d84ee1ccc4edee6b8e04bfc
root <vinz@trans.am>
2019-01-05 03:46:23 +0100

    additional spell packets

---
+++ Source/NexusForever.WorldServer/Network/Message/Model/Server07FA.cs
@@ -0,0 +1,23 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.Server07FA, MessageDirection.Server)]
+    public class Server07FA : IWritable
+    {
+        public uint CastingId { get; set; }
+        public ushort Unknown5 { get; set; }
+        public uint CasterId { get; set; }
+        public bool Unknown10 { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(CastingId);
+            writer.Write(Unknown5, 9);
+            writer.Write(CasterId);
+            writer.Write(Unknown10);
+        }
+    }
+}
+
---
+++ Source/NexusForever.WorldServer/Network/Message/Model/Server07FE.cs
@@ -0,0 +1,17 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.Server07FE, MessageDirection.Server)]
+
+    public class Server07FE : IWritable
+    {
+        public uint CastingId { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(CastingId);
+        }
+    }
+}
