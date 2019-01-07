e70266e674dc620cf053d38c85261bf5c5de9cca
root <vinz@trans.am>
2019-01-07 15:05:34 +0100

    basic spell effect

--- Source/NexusForever.WorldServer/Network/Message/Handler/SpellHandler.cs
+++ Source/NexusForever.WorldServer/Network/Message/Handler/SpellHandler.cs
@@ -34,21 +34,57 @@ namespace NexusForever.WorldServer.Network.Message.Handler
                 Spell4BaseEntry spell4BaseEntry = GameTableManager.Spell4Base.GetEntry(Spell.SpellEntry.Id);
                 if (spell4BaseEntry == null)
                     throw (new InvalidPacketValueException("HandleSpell: Invalid Spell4BaseEntry {Spell.SpellEntry.Id}"));
+
+                Spell4EffectsEntry matchSpell4Effects = Array.Find(GameTableManager.Spell4Effects.Entries, x => x.SpellId == Spell.SpellEntry.Id);
             }
 
             if (spell.Unknown48 == true) // probably "begin casting"
             {
                 Spell4Entry matchSpell4 = Array.Find(GameTableManager.Spell4.Entries, x => x.Spell4BaseIdBaseSpell == Spell.SpellEntry.Id && x.TierIndex == 1);
 
+                // FIXME: this should be a global server increment
+                uint CastingId = (uint)random.Next();
+
                 session.Player.EnqueueToVisible(new Server07FF
                 {
-                    CastingId       = (uint)random.Next(), // FIXME: this should be a global server increment
+                    CastingId       = CastingId,
                     CasterId        = session.Player.Guid,
                     Guid2           = session.Player.Guid, // possibly replace with (if set) target
                     Spell4Id        = matchSpell4.Id,
                     RootSpell4Id    = matchSpell4.Id,
                     ParentSpell4Id  = 0,
                     FieldPosition   = new Position(session.Player.Position),
+                    UserInitiatedSpellCast = true
+                }, true);
+
+                List<Server07F4.UnknownStructure1> unknownStructure1 = new List<Server07F4.UnknownStructure1>
+                {
+                    new Server07F4.UnknownStructure1
+                    {
+                     Spell4EffectId = matchSpell4Effects,
+                     Unknown0 = 4722,
+                     Unknown2 = 4294967295
+                    }
+                };
+
+                List<Server07F4.UnknownStructure0> unknownStructure0 = new List<Server07F4.UnknownStructure0>
+                {
+                    new Server07F4.UnknownStructure0
+                    {
+                        TargetId = session.Player.Guid, // FIXME: insert target
+                        Unknown2 = 1,
+                        Unknown3 = 1,
+                        Unknown4 = 2,
+                        unknownStructure1 = unknownStructure1
+                    }
+                };
+
+                session.Player.EnqueueToVisible(new Server07F4
+                {
+                    CastingId       = CastingId,
+                    Position        = new Position(session.Player.Position),
+                    unknownStructure0 = unknownStructure0,
+                    Unknown1 = 255
                 }, true);
             }
         }
