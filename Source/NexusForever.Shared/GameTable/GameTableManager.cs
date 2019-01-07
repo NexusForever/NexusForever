595fc03c7324e214424f0d61dd6141057ec7eed7
root <vinz@trans.am>
2019-01-07 15:07:34 +0100

    required for spell effects

--- Source/NexusForever.Shared/GameTable/GameTableManager.cs
+++ Source/NexusForever.Shared/GameTable/GameTableManager.cs
@@ -428,7 +428,10 @@ namespace NexusForever.Shared.GameTable
         public static GameTable<Spell4ConditionsEntry> Spell4Conditions { get; private set; }
         public static GameTable<Spell4EffectGroupListEntry> Spell4EffectGroupList { get; private set; }
         public static GameTable<Spell4EffectModificationEntry> Spell4EffectModification { get; private set; }
+
+        [GameData]
         public static GameTable<Spell4EffectsEntry> Spell4Effects { get; private set; }
+
         public static GameTable<Spell4GroupListEntry> Spell4GroupList { get; private set; }
         public static GameTable<Spell4HitResultsEntry> Spell4HitResults { get; private set; }
         public static GameTable<Spell4ModificationEntry> Spell4Modification { get; private set; }
