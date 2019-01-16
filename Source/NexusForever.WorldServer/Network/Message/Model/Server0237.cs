25b7d764374a6e76bf3f2187a6832fb99d183fc3
ahoiahoi <45395388+ahoiahoi@users.noreply.github.com>
2019-01-16 13:51:33 +0100

    Additional spell packets spell damage (#92)
    * Update README.md [ci ckip]
    
    * * addition packets
    * spell damage
    * target selection
    
    * Server07F6 names to match ServerSpellGo (0x07F4)

---
+++ Source/NexusForever.WorldServer/Network/Message/Model/Server0237.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.Server0237, MessageDirection.Server)]
+    public class Server0237 : IWritable
+    {
+        public uint Unknown0 { get; set; } = 0;
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(Unknown0);
+        }
+    }
+}
---
+++ Source/NexusForever.WorldServer/Network/Message/Model/Server0811.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.Server0811, MessageDirection.Server)]
+    public class Server0811 : IWritable
+    {
+        public uint CastingId { get; set; }
+        public List<uint> CasterId { get; set; } = new List<uint>();
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(CastingId);
+            writer.Write(CasterId.Count, 32u);
+            CasterId.ForEach(c => writer.Write(c));
+        }
+    }
+}
