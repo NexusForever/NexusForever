c12f8d8b5d92f3dfa93dfb7c67df1c850d5fe017
Rawaho <rawahodev@gmail.com>
2019-02-15 17:46:30 +1300

    DNS lookup for server address is now done if hostname is supplied instead of an ip address.

--- Source/NexusForever.Shared/Game/ServerManager.cs
+++ Source/NexusForever.Shared/Game/ServerManager.cs
@@ -2,13 +2,17 @@
 using System.Collections.Immutable;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using NexusForever.Shared.Database.Auth;
 using NexusForever.Shared.Database.Auth.Model;
+using NLog;
 
 namespace NexusForever.Shared.Game
 {
     public static class ServerManager
     {
+        private static readonly ILogger log = LogManager.GetCurrentClassLogger();
+
         public class ServerInfo
         {
             public Server Model { get; }
@@ -16,9 +20,24 @@ namespace NexusForever.Shared.Game
 
             public ServerInfo(Server model)
             {
-                IPAddress ipAddress = IPAddress.Parse(model.Host);
-                Address = (uint)IPAddress.HostToNetworkOrder(BitConverter.ToInt32(ipAddress.GetAddressBytes()));
-                Model   = model;
+                try
+                {
+                    if (!IPAddress.TryParse(model.Host, out IPAddress ipAddress))
+                    {
+                        // find first IPv4 address, client doesn't support IPv6 as address is sent as 4 bytes
+                        ipAddress = Dns.GetHostEntry(model.Host)
+                            .AddressList
+                            .First(a => a.AddressFamily == AddressFamily.InterNetwork);
+                    }
+
+                    Address = (uint)IPAddress.HostToNetworkOrder(BitConverter.ToInt32(ipAddress.GetAddressBytes()));
+                    Model   = model;
+                }
+                catch (Exception e)
+                {
+                    log.Fatal(e, $"Failed to load server entry id: {model.Id}, host: {model.Host} from the database!");
+                    throw;
+                }
             }
         }
 
