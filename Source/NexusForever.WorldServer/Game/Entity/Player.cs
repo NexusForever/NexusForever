264ba35489b93b5dc773fa520b3d822795fdc35d
root <vinz@trans.am>
2019-03-09 22:38:03 +0100

    conflicts

--- Source/NexusForever.WorldServer/Game/Entity/Player.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Player.cs
@@ -379,7 +379,7 @@ namespace NexusForever.WorldServer.Game.Entity
                     EntityCommand.SetPlatform,
                     new SetPlatformCommand
                     {
-                        Platform = VehicleGuid
+                        UnitId = VehicleGuid
                     }
                 },
                 {
