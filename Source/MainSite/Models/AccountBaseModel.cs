101ce256c24ecbf93f89a758ea26f056dc31e2fd
VoidEssy <mdknk231@gmail.com>
2019-03-14 23:18:02 +0100

    First Version

---
+++ Source/MainSite/Models/AccountBaseModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainSite.Models
+{
+    public class AccountBaseModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required, DataType(DataType.Password), Compare(nameof(Password))]
+        public string Confirmation { get; set; }
+
+        public AccountBaseModel(string email, string password, string confirmation)
+        {
+            Email = email;
+            Password = password;
+            Confirmation = confirmation;
+        }
+
+        public AccountBaseModel()
+        {
+        }
+    }
+}
