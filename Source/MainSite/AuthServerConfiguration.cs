b385c14789fe70f72517862cc9a16f38703c6902
VoidEssy <mdknk231@gmail.com>
2019-04-07 17:29:23 +0200

    Fixed The DB Error, issue was in initialization of the DB Context. Added a 'okay-ish' fix for now and will look into better solution when code is more cleaned up.

---
+++ Source/MainSite/AuthServerConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using NexusForever.Shared.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainSite
+{
+    public class AuthServerConfiguration
+    {
+        public NetworkConfig Network { get; set; }
+        public DatabaseConfig Database { get; set; }
+    }
+}
