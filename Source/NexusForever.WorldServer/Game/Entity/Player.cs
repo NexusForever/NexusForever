522d3831fc19a91ed5d55c6c11a327cfffe77853
root <vinz@trans.am>
2019-04-25 18:31:19 +0200

    fix for TargetFrame below level 50

--- Source/NexusForever.WorldServer/Game/Entity/Player.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Player.cs
@@ -155,6 +155,7 @@ namespace NexusForever.WorldServer.Game.Entity
 
             // temp
             Properties.Add(Property.BaseHealth, new PropertyValue(Property.BaseHealth, 200f, 800f));
+            Properties.Add(Property.ShieldCapacityMax, new PropertyValue(Property.ShieldCapacityMax, 0f, 450f));
             Properties.Add(Property.MoveSpeedMultiplier, new PropertyValue(Property.MoveSpeedMultiplier, 1f, 1f));
             Properties.Add(Property.JumpHeight, new PropertyValue(Property.JumpHeight, 2.5f, 2.5f));
             Properties.Add(Property.GravityMultiplier, new PropertyValue(Property.GravityMultiplier, 1f, 1f));
