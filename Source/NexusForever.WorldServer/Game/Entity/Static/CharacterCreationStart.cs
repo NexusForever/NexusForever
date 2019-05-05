0384fb37ef213710f8e485b5dadb89e75a32c8bb
root <vinz@trans.am>
2019-05-05 22:11:44 +0200

    due to popular demand :P

--- Source/NexusForever.WorldServer/Game/Entity/Static/CharacterCreationStart.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Static/CharacterCreationStart.cs
@@ -3,11 +3,11 @@ namespace NexusForever.WorldServer.Game.Entity.Static
     public enum CharacterCreationStart
     {
         Arkship         = 0,
-        Demo01,
-        Demo02,
-        Nexus,
-        PreTutorial,
-        Level50,
+        Demo01          = 1,
+        Demo02          = 2,
+        Nexus           = 3,
+        PreTutorial     = 4,
+        Level50         = 5,
         CostumeOnly     = 99
     }
 }
