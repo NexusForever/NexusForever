da8b425e0af746d4133be4d216895646f3c4af98
Tom <tomdymel@web.de>
2019-05-06 17:45:48 +0000

    Moving scripts into a function

--- Tools/Linux/kill.sh
+++
@@ -1,8 +0,0 @@
-echo "Killing NexusForever.StsServer"
-pkill -f NexusForever.StsServer
-
-echo "Killing NexusForever.AuthServer"
-pkill -f NexusForever.AuthServer
-
-echo "Killing NexusForever.WorldServer"
-pkill -f NexusForever.WorldServer
