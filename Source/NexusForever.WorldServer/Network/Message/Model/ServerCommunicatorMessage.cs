46a6a7f731337a6e8c44cb171e4f99326acac9d4
Kirmmin <kirmmin@hotmail.com>
2020-04-11 13:42:36 -0700

    Core/Packets: Client Entered World and Communicators The ClientEnteredWorld packet is sent after every teleport/loading screen. Includes basic Communicator message support. This is used in story telling settings, i.e. quests, dungeons, etc.

---
+++ Source/NexusForever.WorldServer/Network/Message/Model/ServerCommunicatorMessage.cs
@@ -0,0 +1,19 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+using NexusForever.WorldServer.Game.Static;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.ServerCommunicatorMessage)]
+    public class ServerCommunicatorMessage : IWritable
+    {
+        public ushort CommunicatorId { get; set; }
+        public bool Unknown0 { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(CommunicatorId, 15u);
+            writer.Write(Unknown0);
+        }
+    }
+}
