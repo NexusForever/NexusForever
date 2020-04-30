9f09f3d80a7cc0be430ce0281e10b1bd1ba2cd5c
Kirmmin <kirmmin@hotmail.com>
2020-04-29 17:44:39 -0700

    Prerequisite/Checks: Checks Update - April 2020
    Fixes for Quest Prechecks
    
    TargetGroupType added
    
    Add TargetGroups targeting TargetGroups to CreatureCache

--- Source/NexusForever.WorldServer/Game/Prerequisite/Static/PrerequisiteType.cs
+++ Source/NexusForever.WorldServer/Game/Prerequisite/Static/PrerequisiteType.cs
@@ -3,15 +3,18 @@
     // TODO: name these from PrerequisiteType.tbl error messages
     public enum PrerequisiteType
     {
-        None        = 0,
-        Level       = 1,
-        Race        = 2,
-        Class       = 3,
-        Faction     = 4,
-        Reputation  = 5,
-        Quest       = 6,
-        Achievement = 7,
-        Path        = 52,
-        SpellBaseId = 214,
+        None            = 0,
+        Level           = 1,
+        Race            = 2,
+        Class           = 3,
+        Faction         = 4,
+        Reputation      = 5,
+        Quest           = 6,
+        Achievement     = 7,
+        ItemProficiency = 8,
+        Gender          = 10,
+        Prerequisite    = 11,
+        Path            = 52,
+        SpellBaseId     = 214,
     }
 }
