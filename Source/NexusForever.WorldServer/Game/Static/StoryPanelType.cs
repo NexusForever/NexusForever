576e426b010e043b3ddf9faacd3101e2ea008088
Kirmmin <kirmmin@hotmail.com>
2020-05-17 13:09:46 -0700

    Utility/Story: StoryBuilder utility to send lore and story data
    Story Communicator support added

---
+++ Source/NexusForever.WorldServer/Game/Static/StoryPanelType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NexusForever.WorldServer.Game.Static
+{
+    public enum StoryPanelType
+    {
+        Default        = 0,
+        StaticPortrait = 1
+    }
+}
---
+++ Source/NexusForever.WorldServer/Game/Static/WindowType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NexusForever.WorldServer.Game.Static
+{
+    public enum WindowType
+    {
+        LeftAligned  = 0,
+        RightAligned = 1
+    }
+}
