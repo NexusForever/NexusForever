b388c87dcbe5566a0b46c790f75a49c30b7c05ec
Rawaho <rawahodev@gmail.com>
2020-05-24 00:14:31 +1200

    Reworked the way chat message with format links are built.

--- Source/NexusForever.WorldServer/Command/Handler/ItemCommandCategory.cs
+++ Source/NexusForever.WorldServer/Command/Handler/ItemCommandCategory.cs
@@ -1,14 +1,16 @@
-﻿using NexusForever.Shared.GameTable.Model;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NexusForever.Shared.GameTable.Model;
 using NexusForever.WorldServer.Command.Context;
 using NexusForever.WorldServer.Game;
 using NexusForever.WorldServer.Game.Entity;
 using NexusForever.WorldServer.Game.Entity.Static;
 using NexusForever.WorldServer.Game.RBAC.Static;
+using NexusForever.WorldServer.Game.Social;
 using NexusForever.WorldServer.Game.Social.Model;
 using NexusForever.WorldServer.Game.Social.Static;
 using NexusForever.WorldServer.Network.Message.Model.Shared;
-using System.Collections.Generic;
-using System.Linq;
+using NexusForever.WorldServer.Network.Message.Model;
 
 namespace NexusForever.WorldServer.Command.Handler
 {
@@ -33,48 +35,30 @@ namespace NexusForever.WorldServer.Command.Handler
         [Command(Permission.ItemLookup, "Lookup an item by partial name.", "lookup")]
         public void HandleItemLookup(ICommandContext context,
             [Parameter("Item name to lookup.")]
-            string name)
+            string name,
+            [Parameter("Maximum amount of results to return.")]
+            int? maxResults)
         {
-            var searchResults = SearchManager.Instance.Search<Item2Entry>(name, context.Language, e => e.LocalizedTextIdName, true).Take(25);
+            List<Item2Entry> searchResults = SearchManager.Instance
+                .Search<Item2Entry>(name, context.Language, e => e.LocalizedTextIdName, true)
+                .Take(maxResults ?? 25)
+                .ToList();
 
-            if (searchResults.Count() > 0)
+            if (searchResults.Count == 0)
             {
-                context.GetTargetOrInvoker<Player>().Session.EnqueueMessageEncrypted(new Network.Message.Model.ServerChat
-                {
-                    Channel = ChatChannel.System,
-                    Text = $"Item Lookup Results for '{name}' ({searchResults.Count()}):"
-                });
-
-                foreach (Item2Entry itemEntry in searchResults)
-                {
-                    string message = $"({itemEntry.Id}) [I]";
-                    context.GetTargetOrInvoker<Player>().Session.EnqueueMessageEncrypted(new Network.Message.Model.ServerChat
-                    {
-                        Channel = ChatChannel.System,
-                        Text = message,
-                        Formats = new List<ChatFormat>
-                    {
-                        new ChatFormat
-                        {
-                            Type        = ChatFormatType.ItemItemId,
-                            StartIndex  = (ushort)(message.Length - 3),
-                            StopIndex   = (ushort)message.Length,
-                            FormatModel = new ChatFormatItemId
-                            {
-                                ItemId = itemEntry.Id
-                            }
-                        }
-                    }
-                    });
-                }
+                context.SendMessage($"Item lookup results was 0 entries for '{name}'.");
+                return;
             }
-            else
+
+            context.SendMessage($"Item lookup results for '{name}' ({searchResults.Count}):");
+
+            var target = context.GetTargetOrInvoker<Player>();
+            foreach (Item2Entry itemEntry in searchResults)
             {
-                context.GetTargetOrInvoker<Player>().Session.EnqueueMessageEncrypted(new Network.Message.Model.ServerChat
-                {
-                    Channel = ChatChannel.System,
-                    Text = $"Item Lookup Results was 0 entries for '{name}'."
-                });
+                var builder = new ChatMessageBuilder(ChatChannel.System);
+                builder.AppendText($"({itemEntry.Id}) ");
+                builder.AppendItem(itemEntry.Id);
+                target.Session.EnqueueMessageEncrypted(builder.Build());
             }
         }
     }
---
+++ Source/NexusForever.WorldServer/Game/Social/Model/ChatFormatQuestId.cs
@@ -0,0 +1,21 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.WorldServer.Game.Social.Static;
+
+namespace NexusForever.WorldServer.Game.Social.Model
+{
+    [ChatFormat(ChatFormatType.QuestId)]
+    public class ChatFormatQuestId : IChatFormat
+    {
+        public ushort QuestId { get; set; }
+
+        public void Read(GamePacketReader reader)
+        {
+            QuestId = reader.ReadUShort(15u);
+        }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(QuestId, 15u);
+        }
+    }
+}
