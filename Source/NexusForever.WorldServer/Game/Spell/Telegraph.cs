5885e2a5704eca57a7bd8dd845e50ce12ae4e465
kirmmin <32724086+kirmmin@users.noreply.github.com>
2021-02-22 00:13:33 -0500

    Spells/Core: Telegraph additions (Circle, LongCone, Square, Rectangle) (#185)
    * Spells/Core: Telegraph additions (Circle, LongCone, Quadrilateral, Rectangle)
    
    Simplified Circle Telegraph logic
    
    Attempt at Rectangle and Quadrilateral support
    
    Fixes for height checks
    
    Refactored Telegraph logic to be much cleanrer
    
    Refactored the position check to be far cleaner
    Added offsets from the TelegraphDamage entries
    Optimised check where height is a variable
    
    Fixed some accidental spacing changes
    
    Fixed some comments
    
    * Small cleanup.
    
    Co-authored-by: Rawaho <rawahodev@gmail.com>

--- Source/NexusForever.WorldServer/Game/Spell/Telegraph.cs
+++ Source/NexusForever.WorldServer/Game/Spell/Telegraph.cs
@@ -44,7 +44,10 @@ namespace NexusForever.WorldServer.Game.Spell
         {
             switch ((DamageShape)TelegraphDamage.DamageShapeEnum)
             {
+                case DamageShape.Circle:
+                    return Vector3.Distance(Position, position) < TelegraphDamage.Param00;
                 case DamageShape.Cone:
+                case DamageShape.LongCone:
                 {
                     float angleRadian = Position.GetAngle(position);
                     angleRadian -= Rotation.X;
@@ -56,6 +59,51 @@ namespace NexusForever.WorldServer.Game.Spell
 
                     return Vector3.Distance(Position, position) < TelegraphDamage.Param01;
                 }
+                case DamageShape.Quadrilateral:
+                {
+                    float telegraphLength = TelegraphDamage.Param01;
+                    float telegraphHeight = TelegraphDamage.Param02;
+
+                    // TODO: If target is higher or lower than telegraph height, this should not hit. Confirm functionality.
+                    if (position.Y >= Position.Y + telegraphHeight || position.Y <= Position.Y - telegraphHeight)
+                        return false;
+
+                    // TODO: Confirm whether it's necessary to add the model's hit box to the Z Offset (it probably is needed).
+                    Vector3 startingPosition = Position.GetPoint2D(-Rotation.X - MathF.PI / 2, TelegraphDamage.ZPositionOffset);
+                    startingPosition.Y += TelegraphDamage.YPositionOffset;
+
+                    float leftAngle     = -Rotation.X + MathF.PI / 2;
+                    Vector3 bottomLeft  = startingPosition.GetPoint2D(leftAngle, (telegraphLength / 2f) + TelegraphDamage.XPositionOffset);
+                    Vector3 bottomRight = startingPosition.GetPoint2D(leftAngle + MathF.PI / 2, (telegraphLength / 2f) + TelegraphDamage.XPositionOffset);
+                    Vector3 topRight    = bottomRight.GetPoint2D(-Rotation.X - MathF.PI / 4, telegraphLength);
+                    Vector3 topLeft     = bottomLeft.GetPoint2D(-Rotation.X - MathF.PI / 4, telegraphLength);
+
+                    // TODO: Add appropriate check that takes Hit Box from Creature2Model TBL into account with check
+                    return IsPointInsidePolygon(new Vector2(position.X, position.Z), bottomLeft, bottomRight, topRight, topLeft);
+                }
+                case DamageShape.Rectangle:
+                {
+                    float telegraphWidth  = TelegraphDamage.Param00;
+                    float telegraphHeight = TelegraphDamage.Param01;
+                    float telegraphLength = TelegraphDamage.Param02;
+
+                    // TODO: If target is higher or lower than telegraph height, this should not hit. Confirm functionality.
+                    if (position.Y >= Position.Y + telegraphHeight || position.Y <= Position.Y - telegraphHeight)
+                        return false;
+
+                    // TODO: Confirm whether it's necessary to add the model's hit box to the Z Offset (it probably is needed).
+                    Vector3 startingPosition = Position.GetPoint2D(-Rotation.X - MathF.PI / 2, TelegraphDamage.ZPositionOffset);
+                    startingPosition.Y += TelegraphDamage.YPositionOffset;
+
+                    float leftAngle     = -Rotation.X + MathF.PI;
+                    Vector3 bottomLeft  = startingPosition.GetPoint2D(leftAngle, (telegraphWidth / 2f) + TelegraphDamage.XPositionOffset);
+                    Vector3 bottomRight = startingPosition.GetPoint2D(leftAngle + MathF.PI, (telegraphWidth / 2f) + TelegraphDamage.XPositionOffset);
+                    Vector3 topRight    = bottomRight.GetPoint2D(-Rotation.X - MathF.PI / 2, telegraphLength);
+                    Vector3 topLeft     = bottomLeft.GetPoint2D(-Rotation.X - MathF.PI / 2, telegraphLength);
+
+                    // TODO: Add appropriate check that takes Hit Box from Creature2Model TBL into account with check
+                    return IsPointInsidePolygon(new Vector2(position.X, position.Z), bottomLeft, bottomRight, topRight, topLeft);
+                }
                 default:
                     log.Warn($"Unhandled telegraph shape {(DamageShape)TelegraphDamage.DamageShapeEnum}.");
                     return false;
@@ -66,11 +114,43 @@ namespace NexusForever.WorldServer.Game.Spell
         {
             switch ((DamageShape)TelegraphDamage.DamageShapeEnum)
             {
+                case DamageShape.Circle:
+                    return TelegraphDamage.Param00;
                 case DamageShape.Cone:
+                case DamageShape.LongCone:
+                    return TelegraphDamage.Param01;
+                case DamageShape.Quadrilateral:
+                case DamageShape.Rectangle:
                     return TelegraphDamage.Param01;
                 default:
                     return 0f;
             }
+        }
+
+        /// <summary>
+        /// Returns a boolean whether a point sits in a horizontal plane of points.
+        /// </summary>
+        /// <remarks>
+        /// Based on code available in https://github.com/substack/point-in-polygon/ and the algorithm from https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html/
+        /// </remarks>
+        private static bool IsPointInsidePolygon(Vector2 point, params Vector3[] polygon)
+        {
+            bool isInside = false;
+            for (int i = 0, j = polygon.Length- 1; i < polygon.Length; j = i++)
+            {
+                float xi = polygon[i].X;
+                float yi = polygon[i].Z;
+                float xj = polygon[j].X;
+                float yj = polygon[j].Z;
+
+                bool intersect = ((yi > point.Y) != (yj > point.Y))
+                    && (point.X < (xj - xi) * (point.Y - yi) / (yj - yi) + xi);
+
+                if (intersect)
+                    isInside = !isInside;
+            }
+
+            return isInside;
         }
     }
 }
