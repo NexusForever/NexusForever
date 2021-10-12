4d1930f32c540a7bb7d66d450a771c0053004263
Kirmmin <kirmmin@hotmail.com>
2021-10-12 04:55:52 -0700

    Prerequisite/Check: Hoverboard flair support

--- Source/NexusForever.WorldServer/Game/Prerequisite/Static/PrerequisiteType.cs
+++ Source/NexusForever.WorldServer/Game/Prerequisite/Static/PrerequisiteType.cs
@@ -3,17 +3,21 @@
     // TODO: name these from PrerequisiteType.tbl error messages
     public enum PrerequisiteType
     {
-        None        = 0,
-        Level       = 1,
-        Race        = 2,
-        Class       = 3,
-        Faction     = 4,
-        Reputation  = 5,
-        Quest       = 6,
-        Achievement = 7,
-        Path        = 52,
-        Vital       = 73,
-        SpellBaseId = 214,
-        BaseFaction = 250
+        None                = 0,
+        Level               = 1,
+        Race                = 2,
+        Class               = 3,
+        Faction             = 4,
+        Reputation          = 5,
+        Quest               = 6,
+        Achievement         = 7,
+        Path                = 52,
+        Vital               = 73,
+        /// <summary>
+        /// Checks for an ObjectId, which is a hashed petflair id.
+        /// </summary>
+        HoverboardFlair     = 190,
+        SpellBaseId         = 214,
+        BaseFaction         = 250
     }
 }
