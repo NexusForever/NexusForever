9eb5fd4531cc4406235e71c10e165e1b1a14342d
kirmmin <32724086+kirmmin@users.noreply.github.com>
2021-10-12 16:06:06 -0400

    Entity/Mounts: Fix mount summoning (#382)
    Adds support for PreReqs that allow for mounts to be summoned again
    Fixes issue where switching to a different mount, while mounted, was causing disconnects
    
    Co-authored-by: Neiko <rawahodev@gmail.com>

--- Source/NexusForever.WorldServer/Game/Prerequisite/Static/PrerequisiteType.cs
+++ Source/NexusForever.WorldServer/Game/Prerequisite/Static/PrerequisiteType.cs
@@ -3,21 +3,32 @@
     // TODO: name these from PrerequisiteType.tbl error messages
     public enum PrerequisiteType
     {
-        None                = 0,
-        Level               = 1,
-        Race                = 2,
-        Class               = 3,
-        Faction             = 4,
-        Reputation          = 5,
-        Quest               = 6,
-        Achievement         = 7,
-        Path                = 52,
-        Vital               = 73,
-        /// <summary>
+        None            = 0,
+        Level           = 1,
+        Race            = 2,
+        Class           = 3,
+        Faction         = 4,
+        Reputation      = 5,
+        Quest           = 6,
+        Achievement     = 7,
+        Prerequisite    = 11,
+        Path            = 52,
+        Vital           = 73,
+        SpellObj        = 129,
+		    /// <summary>
         /// Checks for an ObjectId, which is a hashed petflair id.
         /// </summary>
-        HoverboardFlair     = 190,
-        SpellBaseId         = 214,
-        BaseFaction         = 250
+        HoverboardFlair = 190,
+        /// <summary>
+        /// Used for Mount checks
+        /// </summary>
+        Unknown194      = 194,
+        /// <summary>
+        /// Used for Mount checks
+        /// </summary>
+        Unknown195      = 195,
+        SpellBaseId     = 214,
+        Plane           = 232,
+        BaseFaction     = 250
     }
 }
