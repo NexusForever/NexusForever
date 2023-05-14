fe6e324bd0abd656069a18d80fd4d17c43e2b1c6
Kirmmin <kirmmin@hotmail.com>
2023-05-14 11:24:37 -0700

    [Script/City] Scripts for Housing Quest & Portal

---
+++ Source/NexusForever.Script.Main/City/Creature/HousingPortal.cs
@@ -0,0 +1,38 @@
+﻿using NexusForever.Game.Abstract.Entity;
+using NexusForever.Script.Template;
+using NexusForever.Script.Template.Filter;
+
+namespace NexusForever.Script.Main.City.Creature
+{
+    // 26350: Housing Hologram
+    [ScriptFilterCreatureId(26350)]
+    public class HousingPortal : ISimpleScript, IOwnedScript<ISimple>
+    {
+        private uint[] SpellTrainingIds =
+        {
+            22919, // Recall - House
+            25520  // Escape House
+        };
+
+        private enum Spells
+        {
+            ConfirmTeleportDialog = 39111
+        }
+
+        private ISimple owner;
+
+        public void Load(ISimple owner)
+        {
+            this.owner = owner;
+        }
+
+        public void OnActivateSuccess(IPlayer activator)
+        {
+            foreach (uint spellBaseId in SpellTrainingIds)
+                if (activator.SpellManager.GetSpell(spellBaseId) == null)
+                    activator.SpellManager.AddSpell(spellBaseId);
+
+            activator.CastSpell((uint)Spells.ConfirmTeleportDialog);
+        }
+    }
+}
---
+++ Source/NexusForever.Script.Main/City/Quest/HousingOfTheFuture.cs
@@ -0,0 +1,151 @@
+﻿using NexusForever.Game.Abstract;
+using NexusForever.Game.Abstract.Entity;
+using NexusForever.GameTable;
+using NexusForever.Script.Template;
+using NexusForever.Script.Template.Filter;
+
+namespace NexusForever.Script.Main.City.Quest
+{
+    public class HousingOfTheFuture
+    {
+        private enum StoryPanelIds
+        {
+            ZenPond        = 2291,
+            PowerGenerator = 2292,
+            Windmill       = 2294,
+            StorageUnit    = 2293
+        }
+
+        // 54400: Zen Pond (Thayd)
+        // 65296: Zen Pond (Illium)
+        [ScriptFilterCreatureId(54400, 65296)]
+        public class ZenPond : ISimpleScript, IOwnedScript<ISimple>
+        {
+            private ISimple owner;
+
+            #region Dependency Injection
+
+            private readonly IStoryBuilder storyBuilder;
+            private readonly IGameTableManager gameTableManager;
+
+            public ZenPond(
+                IStoryBuilder storyBuilder,
+                IGameTableManager gameTableManager)
+            {
+                this.storyBuilder = storyBuilder;
+                this.gameTableManager = gameTableManager;
+            }
+
+            #endregion
+
+            public void OnLoad(ISimple owner)
+            {
+                this.owner = owner;
+            }
+
+            public void OnActivateSuccess(IPlayer activator)
+            {
+                storyBuilder.SendStoryPanel(gameTableManager.StoryPanel.GetEntry(StoryPanelIds.ZenPond), activator);
+            }
+        }
+
+        // 54401: Windmill (Thayd)
+        // 65297: Windmill (Illium)
+        [ScriptFilterCreatureId(54401, 65297)]
+        public class Windmill : ISimpleScript, IOwnedScript<ISimple>
+        {
+            private ISimple owner;
+
+            #region Dependency Injection
+
+            private readonly IStoryBuilder storyBuilder;
+            private readonly IGameTableManager gameTableManager;
+
+            public Windmill(
+                IStoryBuilder storyBuilder,
+                IGameTableManager gameTableManager)
+            {
+                this.storyBuilder = storyBuilder;
+                this.gameTableManager = gameTableManager;
+            }
+
+            #endregion
+
+            public void OnLoad(ISimple owner)
+            {
+                this.owner = owner;
+            }
+
+            public void OnActivateSuccess(IPlayer activator)
+            {
+                storyBuilder.SendStoryPanel(gameTableManager.StoryPanel.GetEntry(StoryPanelIds.Windmill), activator);
+            }
+        }
+
+        // 54403: Power Generator (Thayd)
+        // 65298: Power Generator (Illium)
+        [ScriptFilterCreatureId(54403, 65298)]
+        public class PowerGenerator : ISimpleScript, IOwnedScript<ISimple>
+        {
+            private ISimple owner;
+
+            #region Dependency Injection
+
+            private readonly IStoryBuilder storyBuilder;
+            private readonly IGameTableManager gameTableManager;
+
+            public PowerGenerator(
+                IStoryBuilder storyBuilder,
+                IGameTableManager gameTableManager)
+            {
+                this.storyBuilder = storyBuilder;
+                this.gameTableManager = gameTableManager;
+            }
+
+            #endregion
+
+            public void OnLoad(ISimple owner)
+            {
+                this.owner = owner;
+            }
+
+            public void OnActivateSuccess(IPlayer activator)
+            {
+                storyBuilder.SendStoryPanel(gameTableManager.StoryPanel.GetEntry(StoryPanelIds.PowerGenerator), activator);
+            }
+        }
+
+        // 54404: Storage Unit (Thayd)
+        // 65299: Storage Unit (Illium)
+        [ScriptFilterCreatureId(54404, 65299)]
+        public class StorageUnit : ISimpleScript, IOwnedScript<ISimple>
+        {
+            private ISimple owner;
+
+            #region Dependency Injection
+
+            private readonly IStoryBuilder storyBuilder;
+            private readonly IGameTableManager gameTableManager;
+
+            public StorageUnit(
+                IStoryBuilder storyBuilder,
+                IGameTableManager gameTableManager)
+            {
+                this.storyBuilder = storyBuilder;
+                this.gameTableManager = gameTableManager;
+            }
+
+            #endregion
+
+            public void OnLoad(ISimple owner)
+            {
+                this.owner = owner;
+            }
+
+            public void OnActivateSuccess(IPlayer activator)
+            {
+                storyBuilder.SendStoryPanel(gameTableManager.StoryPanel.GetEntry(StoryPanelIds.StorageUnit), activator);
+            }
+        }
+    }
+}
