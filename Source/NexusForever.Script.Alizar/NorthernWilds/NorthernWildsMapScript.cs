5a2aa635b9f19f4db0b44126bf664d86730181b3
Neal Spellman <nealybealy@comcast.net>
2023-09-19 11:46:17 -0400

    Northern Wilds Cinematic
    Ported Kirmmin's work for the Northern Wilds cinematic and confirmed it plays in-game now.

---
+++ Source/NexusForever.Script.Alizar/NorthernWilds/NorthernWildsMapScript.cs
@@ -0,0 +1,48 @@
+﻿using NexusForever.Game.Abstract.Cinematic.Cinematics;
+using NexusForever.Game.Abstract.Cinematic;
+using NexusForever.Game.Abstract.Entity;
+using NexusForever.Game.Abstract.Map;
+using NexusForever.Script.Template.Filter;
+using NexusForever.Script.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexusForever.Script.Alizar.NorthernWilds
+{
+    [ScriptFilterOwnerId(426)]
+    public class NorthernWildsMapScript : IMapScript, IOwnedScript<IBaseMap>
+    {
+        public enum Quest : ushort
+        {
+            ReportingForDuty = 3480
+        }
+
+        #region Dependency Injection
+
+        private readonly ICinematicFactory cinematicFactory;
+
+        public NorthernWildsMapScript(
+            ICinematicFactory cinematicFactory)
+        {
+            this.cinematicFactory = cinematicFactory;
+        }
+
+        #endregion
+
+        public void OnAddToMap(IGridEntity entity)
+        {
+            if (entity is not IPlayer player)
+                return;
+
+            if (player.QuestManager.GetQuestState(Quest.ReportingForDuty) == null)
+            {
+                player.CinematicManager.QueueCinematic(cinematicFactory.CreateCinematic<INorthernWildsOnCreate>());
+            }
+        }
+
+        // TODO: OnEnterZone work for empowered tower once map script base is ready
+    }
+}
