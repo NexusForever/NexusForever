9163055b59c667c50f3f6bcf1fc97359bd0e838d
Dekker3D <dekker3d@gmail.com>
2023-10-15 08:48:28 +0200

    Just doing distance checks using DistanceSquared() to avoid a slow square root operation.

--- Source/NexusForever.Game/Entity/MailManager.cs
+++ Source/NexusForever.Game/Entity/MailManager.cs
@@ -518,7 +518,7 @@ namespace NexusForever.Game.Entity
                 throw new InvalidOperationException();
 
             var entity = player.GetVisible<IWorldEntity>(unitId);
-            return entity is IMailbox && Vector3.Distance(player.Position, entity.Position) < entry.Datafloat0;
+            return entity is IMailbox && Vector3.DistanceSquared(player.Position, entity.Position) < entry.Datafloat0 * entry.Datafloat0; // Checking squared distance avoids a slow sqrt operation.
         }
     }
 }
