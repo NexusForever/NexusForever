6c5c607784f43c17a6b20df30ac6b5d9ddf6615d
Graeme Hedges <graeme.hedges@gmail.com>
2024-02-26 22:23:50 +0000

    Changed AccountItemEntry fields to properties

--- Source/NexusForever.GameTable/Model/AccountItemEntry.cs
+++ Source/NexusForever.GameTable/Model/AccountItemEntry.cs
@@ -1,23 +1,23 @@
 namespace NexusForever.GameTable.Model
 {
     public class AccountItemEntry
-    {
-        public uint Id;
-        public uint Flags;
-        public uint Item2Id;
-        public uint EntitlementId;
-        public uint EntitlementCount;
-        public uint EntitlementScopeEnum;
-        public uint InstantEventEnum;
-        public uint AccountCurrencyEnum;
-        public ulong AccountCurrencyAmount;
-        public string ButtonIcon;
-        public uint PrerequisiteId;
-        public uint AccountItemCooldownGroupId;
-        public uint StoreDisplayInfoId;
-        public string StoreIdentifierUpsell;
-        public uint Creature2DisplayGroupIdGacha;
-        public uint EntitlementIdPurchase;
-        public uint GenericUnlockSetId;
+    { 
+        public uint Id { get; set; }
+        public uint Flags { get; set; }
+        public uint Item2Id { get; set; }
+        public uint EntitlementId { get; set; }
+        public uint EntitlementCount { get; set; }
+        public uint EntitlementScopeEnum { get; set; }
+        public uint InstantEventEnum { get; set; }
+        public uint AccountCurrencyEnum { get; set; }
+        public ulong AccountCurrencyAmount { get; set; }
+        public string ButtonIcon { get; set; }
+        public uint PrerequisiteId { get; set; }
+        public uint AccountItemCooldownGroupId { get; set; }
+        public uint StoreDisplayInfoId { get; set; }
+        public string StoreIdentifierUpsell { get; set; }
+        public uint Creature2DisplayGroupIdGacha { get; set; }
+        public uint EntitlementIdPurchase { get; set; }
+        public uint GenericUnlockSetId { get; set; }
     }
 }
