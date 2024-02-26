0c5f70dbb422726e4fb308e2bdf061343e52d135
Graeme Hedges <graeme.hedges@gmail.com>
2024-02-26 22:30:56 +0000

    Fixed a find/replace fubar

--- Source/NexusForever.GameTable/Model/AchievementEntry.cs
+++ Source/NexusForever.GameTable/Model/AchievementEntry.cs
@@ -2,28 +2,28 @@ namespace NexusForever.GameTable.Model
 {
     public class AchievementEntry
     {
-        public uint Id { get { get; set; } set { get; set; } }
-        public uint AchievementTypeId { get { get; set; } set { get; set; } }
-        public uint AchievementCategoryId { get { get; set; } set { get; set; } }
-        public uint Flags { get { get; set; } set { get; set; } }
-        public uint WorldZoneId { get { get; set; } set { get; set; } }
-        public uint LocalizedTextIdTitle { get { get; set; } set { get; set; } }
-        public uint LocalizedTextIdDesc { get { get; set; } set { get; set; } }
-        public uint LocalizedTextIdProgress { get { get; set; } set { get; set; } }
-        public float PercCompletionToShow { get { get; set; } set { get; set; } }
-        public uint ObjectId { get { get; set; } set { get; set; } }
-        public uint ObjectIdAlt { get { get; set; } set { get; set; } }
-        public uint Value { get { get; set; } set { get; set; } }
-        public uint CharacterTitleId { get { get; set; } set { get; set; } }
-        public uint PrerequisiteId { get { get; set; } set { get; set; } }
-        public uint PrerequisiteIdServer { get { get; set; } set { get; set; } }
-        public uint PrerequisiteIdObjective { get { get; set; } set { get; set; } }
-        public uint PrerequisiteIdObjectiveAlt { get { get; set; } set { get; set; } }
-        public uint AchievementIdParentTier { get { get; set; } set { get; set; } }
-        public uint OrderIndex { get { get; set; } set { get; set; } }
-        public uint AchievementGroupId { get { get; set; } set { get; set; } }
-        public uint AchievementSubGroupId { get { get; set; } set { get; set; } }
-        public uint AchievementPointEnum { get { get; set; } set { get; set; } }
-        public string SteamAchievementName { get { get; set; } set { get; set; } }
+        public uint Id { get; set; }
+        public uint AchievementTypeId { get; set; }
+        public uint AchievementCategoryId { get; set; }
+        public uint Flags { get; set; }
+        public uint WorldZoneId { get; set; }
+        public uint LocalizedTextIdTitle { get; set; }
+        public uint LocalizedTextIdDesc { get; set; }
+        public uint LocalizedTextIdProgress { get; set; }
+        public float PercCompletionToShow { get; set; }
+        public uint ObjectId { get; set; }
+        public uint ObjectIdAlt { get; set; }
+        public uint Value { get; set; }
+        public uint CharacterTitleId { get; set; }
+        public uint PrerequisiteId { get; set; }
+        public uint PrerequisiteIdServer { get; set; }
+        public uint PrerequisiteIdObjective { get; set; }
+        public uint PrerequisiteIdObjectiveAlt { get; set; }
+        public uint AchievementIdParentTier { get; set; }
+        public uint OrderIndex { get; set; }
+        public uint AchievementGroupId { get; set; }
+        public uint AchievementSubGroupId { get; set; }
+        public uint AchievementPointEnum { get; set; }
+        public string SteamAchievementName { get; set; }
     }
 }
--- Source/NexusForever.GameTable/Model/AchievementGroupEntry.cs
+++ Source/NexusForever.GameTable/Model/AchievementGroupEntry.cs
@@ -2,8 +2,8 @@ namespace NexusForever.GameTable.Model
 {
     public class AchievementGroupEntry
     {
-        public uint Id { get { get; set; } set { get; set; } }
-        public uint LocalizedTextId { get { get; set; } set { get; set; } }
-        public uint TradeSkillId { get { get; set; } set { get; set; } }
+        public uint Id { get; set; }
+        public uint LocalizedTextId { get; set; }
+        public uint TradeSkillId { get; set; }
     }
 }
