28de9e8c1783010e9c8a9e3d2a29662a9570daba
Graeme Hedges <graeme.hedges@gmail.com>
2024-02-26 22:25:32 +0000

    Changed AchievementEntry fields to properties

--- Source/NexusForever.GameTable/Model/AchievementEntry.cs
+++ Source/NexusForever.GameTable/Model/AchievementEntry.cs
@@ -2,28 +2,28 @@ namespace NexusForever.GameTable.Model
 {
     public class AchievementEntry
     {
-        public uint Id;
-        public uint AchievementTypeId;
-        public uint AchievementCategoryId;
-        public uint Flags;
-        public uint WorldZoneId;
-        public uint LocalizedTextIdTitle;
-        public uint LocalizedTextIdDesc;
-        public uint LocalizedTextIdProgress;
-        public float PercCompletionToShow;
-        public uint ObjectId;
-        public uint ObjectIdAlt;
-        public uint Value;
-        public uint CharacterTitleId;
-        public uint PrerequisiteId;
-        public uint PrerequisiteIdServer;
-        public uint PrerequisiteIdObjective;
-        public uint PrerequisiteIdObjectiveAlt;
-        public uint AchievementIdParentTier;
-        public uint OrderIndex;
-        public uint AchievementGroupId;
-        public uint AchievementSubGroupId;
-        public uint AchievementPointEnum;
-        public string SteamAchievementName;
+        public uint Id { get; set; }
+        public uint AchievementTypeId { get; set; }
+        public uint AchievementCategoryId { get; set; }
+        public uint Flags { get; set; }
+        public uint WorldZoneId { get; set; }
+        public uint LocalizedTextIdTitle { get; set; }
+        public uint LocalizedTextIdDesc { get; set; }
+        public uint LocalizedTextIdProgress { get; set; }
+        public float PercCompletionToShow { get; set; }
+        public uint ObjectId { get; set; }
+        public uint ObjectIdAlt { get; set; }
+        public uint Value { get; set; }
+        public uint CharacterTitleId { get; set; }
+        public uint PrerequisiteId { get; set; }
+        public uint PrerequisiteIdServer { get; set; }
+        public uint PrerequisiteIdObjective { get; set; }
+        public uint PrerequisiteIdObjectiveAlt { get; set; }
+        public uint AchievementIdParentTier { get; set; }
+        public uint OrderIndex { get; set; }
+        public uint AchievementGroupId { get; set; }
+        public uint AchievementSubGroupId { get; set; }
+        public uint AchievementPointEnum { get; set; }
+        public string SteamAchievementName { get; set; }
     }
 }
