2199d488743e67cf90cec56eb037183cc6964981
krakal <krakaldev@gmail.com>
2024-05-30 15:04:56 +0100

    Addition of server command to allow changing client's entity send update interval
    Figured out what the unknown parameter is server message 0xF1 (ServerInstanceSettings) does.
    It sets the client's maximum send update interval for the player's unit.
    This same parameter can be changed at any time by sending server message 0x70 (ServerClientEntityUpdateInterval).
    
    Based on captures, the server would always set this parameter as 125 or 126ms and would periodically change it but it is not clear why.

--- Source/NexusForever.Game/Entity/Player.cs
+++ Source/NexusForever.Game/Entity/Player.cs
@@ -634,7 +634,7 @@ namespace NexusForever.Game.Entity
 
             ResidenceManager.SendHousingBasics();
             Session.EnqueueMessageEncrypted(new ServerHousingNeighbors());
-            Session.EnqueueMessageEncrypted(new ServerInstanceSettings() { Unknown3 = 69 });
+            Session.EnqueueMessageEncrypted(new ServerInstanceSettings() { ClientEntityMaxSendUpdateInterval = 125 });
 
             SetControl(this);
 
---
+++ Source/NexusForever.Network.World/Message/Model/ServerEntityUpdateInterval.cs
@@ -0,0 +1,19 @@
+﻿using NexusForever.Network.Message;
+
+namespace NexusForever.Network.World.Message.Model
+{
+    [Message(GameMessageOpcode.ServerClientEntityUpdateInterval)]
+    public class ServerClientEntityUpdateInterval : IWritable
+    {
+        public uint ClientEntityMaxSendUpdateInterval { get; set; }
+        // Interval is specified in milliseconds.
+        // Client will bound the value received between 25 and 1000 ms.
+        // Captured data show that the server would periodically change the interval between 125 and 126 ms
+        // but the reasoning for this is unclear.
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(ClientEntityMaxSendUpdateInterval);
+        }
+    }
+}
