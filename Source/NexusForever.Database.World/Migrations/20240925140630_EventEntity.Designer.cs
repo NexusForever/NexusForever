f8e70e7fda9aefbd8fc91c229fa76abc5dc68bd0
Rawaho <rawahodev@gmail.com>
2024-09-26 02:13:11 +1200

    Added missing database migration for public event entity table.

--- Source/NexusForever.Database.World/Migrations/20240925140630_EventEntity.Designer.cs
+++ Source/NexusForever.Database.World/Migrations/20240925140630_EventEntity.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NexusForever.Database.World;
 
@@ -10,9 +11,11 @@ using NexusForever.Database.World;
 namespace NexusForever.Database.World.Migrations
 {
     [DbContext(typeof(WorldContext))]
-    partial class WorldContextModelSnapshot : ModelSnapshot
+    [Migration("20240925140630_EventEntity")]
+    partial class EventEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/NexusForever.Database.World/Migrations/20240925140630_EventEntity.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NexusForever.Database.World.Migrations
+{
+    /// <inheritdoc />
+    public partial class EventEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "entity_event",
+                columns: table => new
+                {
+                    id = table.Column<uint>(type: "int(10) unsigned", nullable: false, defaultValue: 0u),
+                    eventId = table.Column<uint>(type: "int(10) unsigned", nullable: false, defaultValue: 0u),
+                    phase = table.Column<uint>(type: "int(10) unsigned", nullable: false, defaultValue: 0u)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => new { x.id, x.eventId, x.phase });
+                    table.ForeignKey(
+                        name: "FK__entity_event_id__entity_id",
+                        column: x => x.id,
+                        principalTable: "entity",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entity_event_eventId",
+                table: "entity_event",
+                column: "eventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_entity_event_id",
+                table: "entity_event",
+                column: "id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "entity_event");
+        }
+    }
+}
