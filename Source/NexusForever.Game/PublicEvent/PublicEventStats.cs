5cad244dc5b19b3efe9601994f04de6cab714041
krakal <krakaldev@gmail.com>
2025-09-26 19:41:37 +0100

    * Adds StatToBitDictionary to allow correct mapping of PublicEventStat type to the bit mask in the PublicEventStats bit array

--- Source/NexusForever.Game/PublicEvent/PublicEventStats.cs
+++ Source/NexusForever.Game/PublicEvent/PublicEventStats.cs
@@ -1,10 +1,39 @@
 ﻿using NexusForever.Game.Abstract.PublicEvent;
+using NexusForever.Game.Static.PublicEvent;
 using NetworkPublicEventStats = NexusForever.Network.World.Message.Model.Shared.PublicEventStats;
 
 namespace NexusForever.Game.PublicEvent
 {
     public class PublicEventStats : IPublicEventStats
     {
+        public static readonly Dictionary<PublicEventStat,uint> StatToBitDictionary = new Dictionary<PublicEventStat,uint>()
+        {
+            {PublicEventStat.Damage,0},
+            {PublicEventStat.Hits,1},
+            {PublicEventStat.Haters,2},
+            {PublicEventStat.Kills,3},
+            {PublicEventStat.Deaths,4},
+            {PublicEventStat.Healed,5},
+            {PublicEventStat.Contributions,6},
+            {PublicEventStat.DamageReceived,7},
+            {PublicEventStat.HealingReceived,8},
+            {PublicEventStat.Assists,9},
+            {PublicEventStat.Saves,10},
+            {PublicEventStat.Overhealed,11},
+            {PublicEventStat.OverhealingReceived,12},
+            {PublicEventStat.MedalPoints,13},
+            {PublicEventStat.MaxMultiKill,14},
+            {PublicEventStat.LongestImpulse,15},
+            {PublicEventStat.LongestLife,16},
+            {PublicEventStat.KillStreak,17},
+            {PublicEventStat.CustomStat00,18},
+            {PublicEventStat.CustomStat01,19},
+            {PublicEventStat.CustomStat02,20},
+            {PublicEventStat.CustomStat03,21},
+            {PublicEventStat.CustomStat04,22},
+            {PublicEventStat.CustomStat05,23}
+        };
+
         private readonly Dictionary<Static.PublicEvent.PublicEventStat, uint> stats = [];
         private readonly Dictionary<uint, uint> customStats = [];
 
@@ -30,13 +59,17 @@ namespace NexusForever.Game.PublicEvent
 
             foreach ((Static.PublicEvent.PublicEventStat stat, uint value) in stats.OrderBy(e => e.Key))
             {
-                publicEventStats.Mask.SetBit((uint)stat, true);
+                uint bitNumber = PublicEventStats.StatToBitDictionary[stat];
+
+                publicEventStats.Mask.SetBit(bitNumber, true);
                 publicEventStats.Values.Add(value);
             }
 
             foreach ((uint index, uint value) in customStats.OrderBy(e => e.Key))
             {
-                publicEventStats.Mask.SetBit((uint)Static.PublicEvent.PublicEventStat.CustomStat00 + index, true);
+                uint bitNumber = PublicEventStats.StatToBitDictionary[(PublicEventStat)((uint)Static.PublicEvent.PublicEventStat.CustomStat00 + index)];
+
+                publicEventStats.Mask.SetBit(bitNumber, true);
                 publicEventStats.Values.Add(value);
             }
 
