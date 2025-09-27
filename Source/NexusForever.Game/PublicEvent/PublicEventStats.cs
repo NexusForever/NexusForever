c57e9d819afb2dd745c6862c9b01cf47647d1223
Rawaho <rawahodev@gmail.com>
2025-09-27 19:23:57 +1200

    Small style change.

--- Source/NexusForever.Game/PublicEvent/PublicEventStats.cs
+++ Source/NexusForever.Game/PublicEvent/PublicEventStats.cs
@@ -6,41 +6,41 @@ namespace NexusForever.Game.PublicEvent
 {
     public class PublicEventStats : IPublicEventStats
     {
-        public static readonly Dictionary<PublicEventStat,uint> StatToBitDictionary = new Dictionary<PublicEventStat,uint>()
+        private static readonly Dictionary<PublicEventStat, uint> StatToBitDictionary = new()
         {
-            {PublicEventStat.Damage,0},
-            {PublicEventStat.Hits,1},
-            {PublicEventStat.Haters,2},
-            {PublicEventStat.Kills,3},
-            {PublicEventStat.Deaths,4},
-            {PublicEventStat.Healed,5},
-            {PublicEventStat.Contributions,6},
-            {PublicEventStat.DamageReceived,7},
-            {PublicEventStat.HealingReceived,8},
-            {PublicEventStat.Assists,9},
-            {PublicEventStat.Saves,10},
-            {PublicEventStat.Overhealed,11},
-            {PublicEventStat.OverhealingReceived,12},
-            {PublicEventStat.MedalPoints,13},
-            {PublicEventStat.MaxMultiKill,14},
-            {PublicEventStat.LongestImpulse,15},
-            {PublicEventStat.LongestLife,16},
-            {PublicEventStat.KillStreak,17},
-            {PublicEventStat.CustomStat00,18},
-            {PublicEventStat.CustomStat01,19},
-            {PublicEventStat.CustomStat02,20},
-            {PublicEventStat.CustomStat03,21},
-            {PublicEventStat.CustomStat04,22},
-            {PublicEventStat.CustomStat05,23}
+            { PublicEventStat.Damage,              0 },
+            { PublicEventStat.Hits,                1 },
+            { PublicEventStat.Haters,              2 },
+            { PublicEventStat.Kills,               3 },
+            { PublicEventStat.Deaths,              4 },
+            { PublicEventStat.Healed,              5 },
+            { PublicEventStat.Contributions,       6 },
+            { PublicEventStat.DamageReceived,      7 },
+            { PublicEventStat.HealingReceived,     8 },
+            { PublicEventStat.Assists,             9 },
+            { PublicEventStat.Saves,               10 },
+            { PublicEventStat.Overhealed,          11 },
+            { PublicEventStat.OverhealingReceived, 12 },
+            { PublicEventStat.MedalPoints,         13 },
+            { PublicEventStat.MaxMultiKill,        14 },
+            { PublicEventStat.LongestImpulse,      15 },
+            { PublicEventStat.LongestLife,         16 },
+            { PublicEventStat.KillStreak,          17 },
+            { PublicEventStat.CustomStat00,        18 },
+            { PublicEventStat.CustomStat01,        19 },
+            { PublicEventStat.CustomStat02,        20 },
+            { PublicEventStat.CustomStat03,        21 },
+            { PublicEventStat.CustomStat04,        22 },
+            { PublicEventStat.CustomStat05,        23 }
         };
 
-        private readonly Dictionary<Static.PublicEvent.PublicEventStat, uint> stats = [];
+        private readonly Dictionary<PublicEventStat, uint> stats = [];
         private readonly Dictionary<uint, uint> customStats = [];
 
         /// <summary>
-        /// Update <see cref="Static.PublicEvent.PublicEventStat"/> with supplied value.
+        /// Update <see cref="PublicEventStat"/> with supplied value.
         /// </summary>
-        public void UpdateStat(Static.PublicEvent.PublicEventStat stat, uint value)
+        public void UpdateStat(PublicEventStat stat, uint value)
         {
             stats[stat] = value;
         }
@@ -57,9 +57,9 @@ namespace NexusForever.Game.PublicEvent
         {
             var publicEventStats = new NetworkPublicEventStats();
 
-            foreach ((Static.PublicEvent.PublicEventStat stat, uint value) in stats.OrderBy(e => e.Key))
+            foreach ((PublicEventStat stat, uint value) in stats.OrderBy(e => e.Key))
             {
-                uint bitNumber = PublicEventStats.StatToBitDictionary[stat];
+                uint bitNumber = StatToBitDictionary[stat];
 
                 publicEventStats.Mask.SetBit(bitNumber, true);
                 publicEventStats.Values.Add(value);
@@ -67,7 +67,7 @@ namespace NexusForever.Game.PublicEvent
 
             foreach ((uint index, uint value) in customStats.OrderBy(e => e.Key))
             {
-                uint bitNumber = PublicEventStats.StatToBitDictionary[(PublicEventStat)((uint)Static.PublicEvent.PublicEventStat.CustomStat00 + index)];
+                uint bitNumber = StatToBitDictionary[(PublicEventStat)((uint)PublicEventStat.CustomStat00 + index)];
 
                 publicEventStats.Mask.SetBit(bitNumber, true);
                 publicEventStats.Values.Add(value);
