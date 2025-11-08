28fb732eb4e1325e5d45ded24c0515df85a7a520
krakal <krakaldev@gmail.com>
2025-11-08 21:24:46 +0000

    * Adds messages for character renaming * Adds ServerLoadingScreen message but not in the Pregame namespace as it seems to be used while already in the game

---
+++ Source/NexusForever.Network.World/Message/Model/Pregame/ServerCharacterRenameResult.cs
@@ -0,0 +1,21 @@
+﻿using NexusForever.Network.Message;
+using NexusForever.Network.World.Message.Static;
+
+namespace NexusForever.Network.World.Message.Model.Pregame
+{
+    // Only used by client when on the CharacterSelect screen
+    [Message(GameMessageOpcode.ServerCharacterRenameResult)]
+    public class ServerCharacterRenameResult : IWritable
+    {
+        public CharacterModifyResult Result { get; set; }
+        public ulong CharacterId { get; set; }
+        public string Name { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(Result, 3u);
+            writer.Write(CharacterId);
+            writer.WriteStringWide(Name);
+        }
+    }
+}
