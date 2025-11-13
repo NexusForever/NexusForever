1ca3c86b7c203b17afaa188f953c4e0bca21edec
krakal <krakaldev@gmail.com>
2025-11-13 09:43:22 +0000

    * Adds RecruitmentGuild messages * The data from these messages is accessible from within the UI however Carbine never provided their own user interface that uses the data. An addon could be built to implement an interface to make these messages useful

---
+++ Source/NexusForever.Network.World/Message/Model/Guild/ServerRecruitmentGuildDescription.cs
@@ -0,0 +1,18 @@
+﻿using NexusForever.Network.Message;
+using NexusForever.Network.World.Message.Model.Shared;
+
+namespace NexusForever.Network.World.Message.Model.Guild
+{
+    [Message(GameMessageOpcode.ServerRecruitmentGuildDescription)]
+    public class ServerRecruitmentGuildDescription : IWritable
+    {
+        public Identity GuildIdentity { get; set; }
+        public string Description { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            GuildIdentity.Write(writer);
+            writer.WriteStringWide(Description);
+        }
+    }
+}
---
+++ Source/NexusForever.Network.World/Message/Model/Guild/ServerRecruitmentGuildDetails.cs
@@ -0,0 +1,26 @@
+﻿using NexusForever.Network.Message;
+using NexusForever.Network.World.Message.Model.Shared;
+
+namespace NexusForever.Network.World.Message.Model.Guild
+{
+    [Message(GameMessageOpcode.ServerRecruitmentGuildDetails)]
+    public class ServerRecruitmentGuildDetails : IWritable
+    {
+        public Identity GuildIdentity { get; set; }
+        public string Description { get; set; }
+        public float GuildCreationDaysAgo { get; set; }
+        public bool HasTax { get; set; }
+        public uint RecruitmentMinimumLevel { get; set; }
+        public RecruitmentDemands Demands { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            GuildIdentity.Write(writer);
+            writer.WriteStringWide(Description);
+            writer.Write(GuildCreationDaysAgo);
+            writer.Write(HasTax);
+            writer.Write(RecruitmentMinimumLevel);
+            Demands.Write(writer);
+        }
+    }
+}
