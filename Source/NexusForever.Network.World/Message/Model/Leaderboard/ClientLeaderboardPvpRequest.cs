c12ef886e761ef9a901d336739e9cbe238e9ae62
krakal <krakaldev@gmail.com>
2025-11-17 19:20:54 +0000

    * Adds Leaderboard namespace * Adds all Leaderboard messages

---
+++ Source/NexusForever.Network.World/Message/Model/Leaderboard/ClientLeaderboardPvpRequest.cs
@@ -0,0 +1,16 @@
+﻿using NexusForever.Game.Static.Leaderboard;
+using NexusForever.Network.Message;
+
+namespace NexusForever.Network.World.Message.Model.Loot
+{
+    [Message(GameMessageOpcode.ClientLeaderboardPvpRequest)]
+    public class ClientLeaderboardPvpRequest : IReadable
+    {
+        public LeaderboardType Type { get; private set; }
+
+        public void Read(GamePacketReader reader)
+        {
+            Type = reader.ReadEnum<LeaderboardType>(4u);
+        }
+    }
+}
---
+++ Source/NexusForever.Network.World/Message/Model/Leaderboard/TeamMember.cs
@@ -0,0 +1,17 @@
+﻿using NexusForever.Game.Static.Entity;
+using NexusForever.Network.Message;
+
+namespace NexusForever.Network.World.Message.Model.Leaderboard
+{
+    public class TeamMember : IWritable
+    {
+        public string Name { get; set; }
+        public Class Class { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.WriteStringWide(Name);
+            writer.Write(Class, 5u);
+        }
+    }
+}
