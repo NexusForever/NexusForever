601f7555dfe58ae952bc7ed8fe18efca109fbb6c
Musaji <mcipolla@asu.edu>
2025-11-30 17:31:53 -0700

    who-command-musaji - Added ClientWhoRequestHandler implementation to filter results based   on filtering criteria stored in the ClientWhoRequest object.
    - This commit deals primarily with the WhoParameterCombo object. With this who request subtype, we
      filter who request criteria based on strings, which will either map to
      a substring in a name, or a byte encoding for Race and Class. The two
      zone id encodings as well as path encodings are not supported by this
      commit.
    
    - Right now, who results include the player initiating the request in
      their output. This can be easily prevented by checking the session
      data, but I wasn't sure what the expected behavior for this command
      was.
    
    who-command-musaji
    - This commit implements the query by PathId functionality for the
      WhoParameterCombo object.
    
    - New none types were added to the relevant byte enums that encoded the
      combo data. This is necessary to avoid comparing against magic numbers
      that could potentially mean something (examples: 0, 4).
    
    - I added a readonly bool to allow for easy toggling of whether or not
      the intitiating player is included in the search results. I added this
      since the expected behavior wasn't clear to me.
    
    - Additionally, I refactored some of the naming that I thought was
      unclear / too generic.
    
    who-command-musaji
    - Added some code to prevent members of the opposite faction from
      appearing in the search window. I added a readonly variable to turn
      this functionality off for now.
    
    who-command-musaji
    - Implemented zone checking by comparing the Player.Zone.ZoneId with the
      Combo request data WorldZoneId.
    
    - Not sure what to do with WorldZoneId2. Comment indicates that this
      property is usually just a duplicate of the WorldZoneId. This
      indicates to me that WorldZoneId is always correct and WorldZoneId2 is
      only sometimes correct, so we do not read from id2.
    
    who-command-musaji
    - Added support / implementation for who filtering based on Level and
      Level range.
    
    who-command-musaji
    - Did a refactoring pass. I'm ultimately unhappy with the structure. We
      have no support for logical operator groups, and there's alot of
      control statements.
    
    - Support for type commands has been added. All 9 Who types are
      supported and have been tested. Subzoning still isn't supported, even
      with the explicit zone type being used and quotes being used. Not sure how
      we can accomplish this if client isn't sending subzone strings.
    
    - Still have a to investigate a thread safe manager to collect player
      data, and we need to further investigate the support of concatenating
      logical operations for more complex who queries.
    
    who-command-musaji
    - Implemented logical predicate combinations and applied them with a
      reducer.
    
    who-command-musaji
    - Added new WIP tags and removed unnecessary comments and code. Going to
      start working through the final bits of implementation.
    
    who-command-musaji
    - I read over PlayerManager and it seemed to be a thread safe version of
      the world sessions I was using to collect players from. I've opted to
      use this and check to see if this data structure fits the requirements
      of the maintainers of the project.
    
    - I refactored the code via some naming updates, and structurally
      rearranged alot of the elements I had originally.
    
    - I've left WIP markers on areas that I am unsure about. I will make
      note of these in the discord to collect input from the other
      developers.
    
    who-command-musaji
    - Minor rename / comment removal. Added documentation to public API.

--- Source/NexusForever.Game.Static/Entity/Path.cs
+++ Source/NexusForever.Game.Static/Entity/Path.cs
@@ -2,9 +2,10 @@
 {
     public enum Path : byte
     {
-        Soldier,
-        Settler,
-        Scientist,
-        Explorer
+        None = 4, // The value for data that does not contain a Path byte resolves to 4. Adding it at the top to communicate it should not be changed.
+        Soldier = 0,
+        Settler = 1,
+        Scientist = 2,
+        Explorer = 3,
     }
 }
--- Source/NexusForever.WorldServer/Network/Message/Handler/Chat/ClientWhoRequestHandler.cs
+++ Source/NexusForever.WorldServer/Network/Message/Handler/Chat/ClientWhoRequestHandler.cs
@@ -1,31 +1,311 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Extensions.DependencyInjection;
+using NexusForever.Game.Abstract.Entity;
+using NexusForever.Game.Entity;
+using NexusForever.Game.Static.Entity;
+using NexusForever.Game.Static.Who;
 using NexusForever.Network.Message;
+using NexusForever.Network.Session;
 using NexusForever.Network.World.Message.Model.Who;
+using NexusForever.Network.World.Message.Model.Who.Parameter;
+using NexusForever.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NexusForever.WorldServer.Network.Message.Handler.Chat
 {
     public class ClientWhoRequestHandler : IMessageHandler<IWorldSession, ClientWhoRequest>
     {
-        public void HandleMessage(IWorldSession session, ClientWhoRequest request)
+        private enum ComboFilterStrategy
         {
-            var players = new List<ServerWhoResponse.WhoPlayer>
+            Name,
+            Race,
+            Class,
+            Path,
+            Zone,
+            Level
+        }
+        
+        private ClientWhoRequest clientWhoRequest;
+        private Dictionary<WhoParameterType, Func<WhoParameter, IPlayer, bool>> queryToFunctionMapping;
+
+        /// <summary>
+        /// This method interprets the ClientWhoRequest object and returns a list of players who's player attributes match the search query.
+        /// </summary>
+        /// <param name="requestingSession">IWorldSession information mapped to the player who initiated the who request.</param>
+        /// <param name="request"> ClientWhoRequest object that stores the who request parameters that the server received from the client.</param>
+        public void HandleMessage(IWorldSession requestingSession, ClientWhoRequest request)
+        {
+            // WIP: Rāwaho suggested that I use some kind of manager that's thread safe for this operation.
+            // WIP: It seems like the PlayerManager is exactly a threadsafe collection of players? Need I do anything more?
+            List<IPlayer> playerSessions = PlayerManager.Instance.ToList();
+
+            List<ServerWhoResponse.WhoPlayer> whoResponsePlayerList = new List<ServerWhoResponse.WhoPlayer>();
+            WhoParameter? currentRequestParameter = request.Parameters.Count > 0 ? request.Parameters[0] : null;
+            clientWhoRequest = request;
+
+            queryToFunctionMapping = new Dictionary<WhoParameterType, Func<WhoParameter, IPlayer, bool>>
             {
-                new()
-                {
-                    Name    = session.Player.Name,
-                    Level   = session.Player.Level,
-                    Race    = session.Player.Race,
-                    Class   = session.Player.Class,
-                    Path    = session.Player.Path,
-                    Faction = session.Player.Faction1,
-                    Sex     = session.Player.Sex,
-                    Zone    = session.Player.Zone.Id
-                }
+                { WhoParameterType.Level, FilterByLevel },
+                { WhoParameterType.Race, FilterByRace },
+                { WhoParameterType.Path, FilterByPath },
+                { WhoParameterType.Class, FilterByClass },
+                { WhoParameterType.Zone, FilterByZone },
+                { WhoParameterType.Player, FilterByPlayer },
+                { WhoParameterType.Guild, FilterByGuild },
+                { WhoParameterType.Faction, FilterByFaction },
+                { WhoParameterType.Combo, FilterByCombo }
             };
 
-            session.EnqueueMessageEncrypted(new ServerWhoResponse
+            playerSessions.Aggregate(whoResponsePlayerList, AggregateFilter);
+
+            requestingSession.EnqueueMessageEncrypted(new ServerWhoResponse
             {
-                Players = players
+                Players = whoResponsePlayerList
+            });
+        }
+
+        private List<ServerWhoResponse.WhoPlayer> AggregateFilter(List<ServerWhoResponse.WhoPlayer> accumulator, IPlayer playerCandidate)
+        {
+            Func<WhoParameter, IPlayer, bool>[] queryFunctions = new Func<WhoParameter, IPlayer, bool>[clientWhoRequest.Parameters.Count];
+            
+            for (int i = 0; i < clientWhoRequest.Parameters.Count; i++)
+            {
+                WhoParameter whoParameter = clientWhoRequest.Parameters[i];
+                queryFunctions[i] = queryToFunctionMapping[whoParameter.Type];
+            }
+
+            bool combinedQueryOutcome = false;
+
+            int lastIndex = 0;
+            for (int i = 0; i <= clientWhoRequest.ParameterGroupCounts.Count; i++)
+            {
+                bool singleQueryGroupValue = true;
+                int targetIndex = i == clientWhoRequest.ParameterGroupCounts.Count ? clientWhoRequest.Parameters.Count : clientWhoRequest.ParameterGroupCounts[i];
+                    
+                for (int j = lastIndex; j < targetIndex; j++)
+                {
+                    if (queryFunctions[j](clientWhoRequest.Parameters[j], playerCandidate) == false)
+                    {
+                        singleQueryGroupValue = false;
+                        break;
+                    }
+                }
+
+                lastIndex = targetIndex;
+
+                if (singleQueryGroupValue == true)
+                {
+                    combinedQueryOutcome = true;
+                    break;
+                }
+            }
+
+            if (combinedQueryOutcome == true)
+            {
+                AddPlayerToList(playerCandidate, accumulator);
+            }
+
+            return accumulator;
+        }
+
+        private bool FilterByLevel(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            WhoParameterLevel levelData = whoParameter.Data as WhoParameterLevel;
+            uint candidateLevel = playerCandidate.Level;
+            return candidateLevel >= levelData.BottomLevel && candidateLevel < levelData.TopLevel;
+        }
+
+        private bool FilterByRace(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            Race? raceId = null;
+            if (whoParameter.Type == WhoParameterType.Race)
+            {
+                WhoParameterRace raceData = whoParameter.Data as WhoParameterRace;
+                raceId = raceData.RaceId;
+            }
+            else if (whoParameter.Type == WhoParameterType.Combo)
+            {
+                WhoParameterCombo comboData = whoParameter.Data as WhoParameterCombo;
+                raceId = comboData.RaceId;
+            }
+
+            return playerCandidate.Race == raceId;
+        }
+
+        private bool FilterByPath(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            Game.Static.Entity.Path? pathId = null;
+            if (whoParameter.Type == WhoParameterType.Path)
+            {
+                WhoParameterPath pathData = whoParameter.Data as WhoParameterPath;
+                pathId = pathData.PathId;
+            }
+            else if (whoParameter.Type == WhoParameterType.Combo)
+            {
+                WhoParameterCombo comboData = whoParameter.Data as WhoParameterCombo;
+                pathId = comboData.PathId;
+            }
+
+            return playerCandidate.Path == pathId;
+        }
+
+        private bool FilterByClass(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            Class? classId = null;
+            if (whoParameter.Type == WhoParameterType.Class)
+            {
+                WhoParameterClass classData = whoParameter.Data as WhoParameterClass;
+                classId = classData.ClassId;
+            }
+            else if (whoParameter.Type == WhoParameterType.Combo)
+            {
+                WhoParameterCombo comboData = whoParameter.Data as WhoParameterCombo;
+                classId = comboData.ClassId;
+            }
+
+            return playerCandidate.Class == classId;
+        }
+
+        private bool FilterByZone(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            uint? zoneId = null;
+            if (whoParameter.Type == WhoParameterType.Zone)
+            {
+                WhoParameterZone zoneData = whoParameter.Data as WhoParameterZone;
+                zoneId = zoneData.WorldZoneId;
+            }
+            else if (whoParameter.Type == WhoParameterType.Combo)
+            {
+                WhoParameterCombo comboData = whoParameter.Data as WhoParameterCombo;
+                zoneId = comboData.WorldZoneId;
+            }
+
+            // WIP: It's not clear how ComboWhoRequests interact with the subzones. I have not been able to get a combo sub-zone request through via the client.
+            // For now, if you're in a subzone, we will compare your parent zone id with the search query, since combo search querys seem to only support these.
+            uint subZoneId = playerCandidate.Zone.Id;
+            uint parentZoneId = playerCandidate.Zone.ParentZoneId;
+            if (parentZoneId == 0)
+            {
+                return playerCandidate.Zone.Id == zoneId;
+            }
+            else
+            {
+                return playerCandidate.Zone.ParentZoneId == zoneId;
+            }
+        }
+
+        private bool FilterByPlayer(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            string? filterString = null;
+            if (whoParameter.Type == WhoParameterType.Player)
+            {
+                WhoParameterPlayer playerData = whoParameter.Data as WhoParameterPlayer;
+                filterString = playerData.PlayerName;
+            }
+            else if(whoParameter.Type == WhoParameterType.Combo)
+            {
+                WhoParameterCombo comboData = whoParameter.Data as WhoParameterCombo;
+                filterString = comboData.SearchString;
+            }
+
+            string candidateName = playerCandidate.Name;
+
+            return StringFilter(candidateName, filterString);
+        }
+
+        private bool FilterByGuild(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            WhoParameterGuild guildData = whoParameter.Data as WhoParameterGuild;
+
+            if (playerCandidate.GuildManager.Guild == null)
+            {
+                return false;
+            }
+
+            string candidateName = playerCandidate.GuildManager.Guild.Name;
+            string filterString = guildData.GuildName;
+
+            return StringFilter(candidateName, filterString);
+        }
+
+        private static bool StringFilter(string candidateName, string filterString)
+        {
+            // WIP: Should we handle string lengths < 3?
+            //if (playerData.PlayerName.Length < 3)
+            //{
+            //    return false;
+            //}
+
+            string lowerFilterString = filterString.ToLower();
+            string lowerCandidateName = candidateName.ToLower();
+
+            if (lowerCandidateName.IndexOf(lowerFilterString) != -1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool FilterByFaction(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            WhoParameterFaction factionData = whoParameter.Data as WhoParameterFaction;
+            return playerCandidate.Faction2 == factionData.Faction2Id;
+        }
+
+        private bool FilterByCombo(WhoParameter whoParameter, IPlayer playerCandidate)
+        {
+            WhoParameterCombo comboData = whoParameter.Data as WhoParameterCombo;
+            ComboFilterStrategy inferredFilterStrategy = ComboFilterStrategy.Name;
+
+            if (comboData.RaceId != Race.None)
+            {
+                inferredFilterStrategy = ComboFilterStrategy.Race;
+            }
+            else if (comboData.ClassId != Class.None)
+            {
+                inferredFilterStrategy = ComboFilterStrategy.Class;
+            }
+            else if (comboData.PathId != Game.Static.Entity.Path.None)
+            {
+                inferredFilterStrategy = ComboFilterStrategy.Path;
+            }
+            else if (comboData.WorldZoneId != 0)
+            {
+                inferredFilterStrategy = ComboFilterStrategy.Zone;
+            }
+
+            switch(inferredFilterStrategy)
+            {
+                case ComboFilterStrategy.Name:
+                    return FilterByPlayer(whoParameter, playerCandidate);
+                case ComboFilterStrategy.Race:
+                    return FilterByRace(whoParameter, playerCandidate);
+                case ComboFilterStrategy.Class:
+                    return FilterByClass(whoParameter, playerCandidate);
+                case ComboFilterStrategy.Path:
+                    return FilterByPath(whoParameter, playerCandidate);
+                case ComboFilterStrategy.Zone:
+                    return FilterByZone(whoParameter, playerCandidate);
+                default:
+                    return false;
+            }
+        }
+
+
+        private void AddPlayerToList(IPlayer playerCandidate, List<ServerWhoResponse.WhoPlayer> whoResponsePlayerList)
+        {
+            whoResponsePlayerList.Add(new()
+            {
+                Name = playerCandidate.Name,
+                Level = playerCandidate.Level,
+                Race = playerCandidate.Race,
+                Class = playerCandidate.Class,
+                Path = playerCandidate.Path,
+                Faction = playerCandidate.Faction1,
+                Sex = playerCandidate.Sex,
+                Zone = playerCandidate.Zone.Id
             });
         }
     }
