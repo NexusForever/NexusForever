757b2c98472f890b40299a5ac855e1f35579df62
Musaji <mcipolla@asu.edu>
2025-11-30 17:31:53 -0700

    who-command-musaji - Added ClientWhoRequestHandler implementation to filter results based   on filtering criteria stored in the ClientWhoRequest object.
    - This commit deals primarily with the WhoParameterCombo object. With this who request subtype, we
      filter who request criteria based on strings, which will either map to
      a substring in a name, or a byte encoding for Race and Class. The two
      zone id encodings as well as path encodings are not supported by this
      commit.
    
    - Right now, who results include the player initiating the request in
      their output. This can be easily prevented by checking the session
      data, but I wasn't sure what the expected behavior for this command
      was.

--- Source/NexusForever.WorldServer/Network/Message/Handler/Chat/ClientWhoRequestHandler.cs
+++ Source/NexusForever.WorldServer/Network/Message/Handler/Chat/ClientWhoRequestHandler.cs
@@ -1,31 +1,114 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualBasic;
+using NexusForever.Game.Static.Entity;
 using NexusForever.Network.Message;
+using NexusForever.Network.Session;
 using NexusForever.Network.World.Message.Model.Who;
+using NexusForever.Network.World.Message.Model.Who.Parameter;
+using NexusForever.Shared;
+using System.Collections.Generic;
 
 namespace NexusForever.WorldServer.Network.Message.Handler.Chat
 {
     public class ClientWhoRequestHandler : IMessageHandler<IWorldSession, ClientWhoRequest>
     {
+
+        private enum FilterStrategy
+        {
+            Name,
+            Race,
+            Class,
+        }
+
+        private FilterStrategy inferredFilterStrategy;
         public void HandleMessage(IWorldSession session, ClientWhoRequest request)
         {
-            var players = new List<ServerWhoResponse.WhoPlayer>
+            INetworkManager<IWorldSession> worldSessions = LegacyServiceProvider.Provider.GetService<INetworkManager<IWorldSession>>();
+
+            var players = new List<ServerWhoResponse.WhoPlayer>();
+            var currentRequestParameter = request.Parameters.Count > 0 ? request.Parameters[0] : null;
+
+            // Iterate over sessions for filtering.
+            foreach (var sessionKey in worldSessions)
             {
-                new()
+                var sessionPlayer = sessionKey.Player;
+
+                if (currentRequestParameter == null)
                 {
-                    Name    = session.Player.Name,
-                    Level   = session.Player.Level,
-                    Race    = session.Player.Race,
-                    Class   = session.Player.Class,
-                    Path    = session.Player.Path,
-                    Faction = session.Player.Faction1,
-                    Sex     = session.Player.Sex,
-                    Zone    = session.Player.Zone.Id
+                    AddPlayerToList(players, sessionPlayer);
                 }
-            };
+                else if (currentRequestParameter.Type == Game.Static.Who.WhoParameterType.Combo)
+                {
+                    WhoParameterCombo comboData = currentRequestParameter.Data as WhoParameterCombo;
+                    FilterByCombo(players, sessionPlayer, comboData);
+                }
+            }
 
             session.EnqueueMessageEncrypted(new ServerWhoResponse
             {
                 Players = players
+            });
+        }
+
+        private void FilterByCombo(List<ServerWhoResponse.WhoPlayer> players, Game.Abstract.Entity.IPlayer sessionPlayer, WhoParameterCombo comboData)
+        {
+            inferredFilterStrategy = FilterStrategy.Name;
+
+            if (comboData.RaceId != Race.None)
+            {
+                inferredFilterStrategy = FilterStrategy.Race;
+            }
+            else if (comboData.ClassId != 0)
+            {
+                inferredFilterStrategy = FilterStrategy.Class;
+            }
+
+            switch (inferredFilterStrategy) 
+            {
+                case FilterStrategy.Name:
+                    FilterByName(players, sessionPlayer, comboData.SearchString, sessionPlayer.Name);
+                    break;
+                case FilterStrategy.Race:
+                    FilterByArgs(players, sessionPlayer, comboData.RaceId, sessionPlayer.Race);
+                    break;
+                case FilterStrategy.Class:
+                    FilterByArgs(players, sessionPlayer, comboData.ClassId, sessionPlayer.Class);
+                    break;
+            }
+        }
+
+        private void FilterByArgs<T>(List<ServerWhoResponse.WhoPlayer> players, Game.Abstract.Entity.IPlayer sessionPlayer, T filterValue, T sessionValue)
+        {
+            if (EqualityComparer<T>.Default.Equals(filterValue, sessionValue))
+            {
+                AddPlayerToList(players, sessionPlayer);
+            }
+        }
+
+        private void FilterByName(List<ServerWhoResponse.WhoPlayer> players, Game.Abstract.Entity.IPlayer sessionPlayer, string comboSearchString, string sessionName)
+        {
+            // We want to filter in a case insensitive way
+            string lowercaseSearch = comboSearchString.ToLower();
+            string lowercaseSessionName = sessionName.ToLower();
+
+            if (lowercaseSessionName.IndexOf(lowercaseSearch) != -1)
+            {
+                AddPlayerToList(players, sessionPlayer);
+            }
+        }
+
+        private static void AddPlayerToList(List<ServerWhoResponse.WhoPlayer> players, Game.Abstract.Entity.IPlayer sessionPlayer)
+        {
+            players.Add(new()
+            {
+                Name = sessionPlayer.Name,
+                Level = sessionPlayer.Level,
+                Race = sessionPlayer.Race,
+                Class = sessionPlayer.Class,
+                Path = sessionPlayer.Path,
+                Faction = sessionPlayer.Faction1,
+                Sex = sessionPlayer.Sex,
+                Zone = sessionPlayer.Zone.Id
             });
         }
     }
