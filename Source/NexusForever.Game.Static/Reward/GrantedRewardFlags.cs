ce95068ea0bd5c1a4f2a19d8c0adcfcf64be3165
krakal <krakaldev@gmail.com>
2025-12-11 10:16:10 +0000

    Update GrantedRewardFlags.cs
    * Renamed flags to be more specific and removed overlapping flag values

--- Source/NexusForever.Game.Static/Reward/GrantedRewardFlags.cs
+++ Source/NexusForever.Game.Static/Reward/GrantedRewardFlags.cs
@@ -3,9 +3,8 @@
     [Flags]
     public enum GrantedRewardFlags : uint
     {
-        Item     = 0x00000001,
-        Modifier = 0x00000001,
-        Essence  = 0x80000000,
+        GrantItemsOrModifiers = 0x00000001,
+        GrantAllTypes         = 0x80000000,
 
         // Carbine servers seemed to send more flags than these but the client doesn't appear to use them
     }
